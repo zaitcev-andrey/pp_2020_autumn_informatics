570217884b822c994a747c857c9453ee96839c28
Nesterov Alexander <nesoldr@mail.ru>
2020-10-08 06:18:39 +0300

    Update task

--- modules/task_1/nesterov_a_vector_sum/ops_mpi.h
+++ modules/task_1/nesterov_a_vector_sum/ops_mpi.h
@@ -1,6 +1,6 @@
 // Copyright 2018 Nesterov Alexander
-#ifndef MODULES_TEST_TASKS_TEST_MPI_OPS_MPI_H_
-#define MODULES_TEST_TASKS_TEST_MPI_OPS_MPI_H_
+#ifndef MODULES_TASK_1_NESTEROV_A_VECTOR_SUM_OPS_MPI_H_
+#define MODULES_TASK_1_NESTEROV_A_VECTOR_SUM_OPS_MPI_H_
 
 #include <vector>
 #include <string>
@@ -10,4 +10,4 @@ int getParallelOperations(std::vector<int> global_vec,
                           int count_size_vector, std::string ops);
 int getSequentialOperations(std::vector<int> vec, std::string ops);
 
-#endif  // MODULES_TEST_TASKS_TEST_MPI_OPS_MPI_H_
+#endif  // MODULES_TASK_1_NESTEROV_A_VECTOR_SUM_OPS_MPI_H_
