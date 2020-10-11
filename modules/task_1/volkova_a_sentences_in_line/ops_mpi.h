2e1363cd22e1c4e6db67b6235e50a87954f22016
WolfCub15 <volkovanastyavolkova@gmail.com>
2020-10-11 21:21:29 +0300

    delete

--- modules/task_1/volkova_a_sentences_in_line/ops_mpi.h
+++
@@ -1,7 +0,0 @@
-
-#include <vector>
-#include <string>
-
-std::vector<char> getRandomLine(int  size);
-int getParallelOperations(std::vector<char> global_vec, int count_size_sentence);
-int getSequentialOperations(std::vector<char> sentence);
