2332eff5684b283898d01114ef63caa5f7b5a6d5
Mikhail-Zoreev <43145054+Mikhail-Zoreev@users.noreply.github.com>
2020-10-14 22:24:45 +0300

    std::functions support test

--- modules/task_1/zoreev_m_rectangle_method/main.cpp
+++ modules/task_1/zoreev_m_rectangle_method/main.cpp
@@ -11,9 +11,10 @@ TEST(Parallel_Operations_MPI, INTEGRAL_FROM_0_TO_1) {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    double parallel_result = integralParallel(0, 1, 10000);
+    
+    double parallel_result = integralParallel(static_cast<double(*)(double)>(&cos), 0, 1, 10000);
     if (rank == 0) {
-        double sequential_result = integralSeqential(0, 1, 10000);
+        double sequential_result = integralSeqential(static_cast<double(*)(double)>(&cos), 0, 1, 10000);
         ASSERT_LT(std::fabs(parallel_result - sequential_result), std::numeric_limits<double>::epsilon() * 100);
     }
 }
@@ -22,9 +23,9 @@ TEST(Parallel_Operations_MPI, INTEGRAL_FROM_5_TO_0) {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    double parallel_result = integralParallel(5, 0, 10000);
+    double parallel_result = integralParallel(static_cast<double(*)(double)>(&cos), 5, 0, 10000);
     if (rank == 0) {
-        double sequential_result = integralSeqential(5, 0, 10000);
+        double sequential_result = integralSeqential(static_cast<double(*)(double)>(&cos), 5, 0, 10000);
         ASSERT_LT(std::fabs(parallel_result - sequential_result), std::numeric_limits<double>::epsilon() * 100);
     }
 }
@@ -33,9 +34,9 @@ TEST(Parallel_Operations_MPI, INTEGRAL_FROM_0_TO_100) {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    double parallel_result = integralParallel(0, 100, 10000);
+    double parallel_result = integralParallel(static_cast<double(*)(double)>(&cos), 0, 100, 10000);
     if (rank == 0) {
-        double sequential_result = integralSeqential(0, 100, 10000);
+        double sequential_result = integralSeqential(static_cast<double(*)(double)>(&cos), 0, 100, 10000);
         ASSERT_LT(std::fabs(parallel_result - sequential_result), std::numeric_limits<double>::epsilon() * 100);
     }
 }
@@ -44,9 +45,9 @@ TEST(Parallel_Operations_MPI, INTEGRAL_FROM_0_TO_709) {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    double parallel_result = integralParallel(0, 709, 10000);
+    double parallel_result = integralParallel(static_cast<double(*)(double)>(&cos), 0, 709, 10000);
     if (rank == 0) {
-        double sequential_result = integralSeqential(0, 709, 10000);
+        double sequential_result = integralSeqential(static_cast<double(*)(double)>(&cos), 0, 709, 10000);
         ASSERT_LT(std::fabs(parallel_result - sequential_result), std::numeric_limits<double>::epsilon() * 100);
     }
 }
@@ -55,15 +56,15 @@ TEST(Parallel_Operations_MPI, INTEGRAL_WITH_LOW_RANGE) {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    double parallel_result = integralParallel(1, 1.01, 10000);
+    double parallel_result = integralParallel(static_cast<double(*)(double)>(&cos), 1, 1.01, 10000);
     if (rank == 0) {
-        double sequential_result = integralSeqential(1, 1.01, 10000);
+        double sequential_result = integralSeqential(static_cast<double(*)(double)>(&cos), 1, 1.01, 10000);
         ASSERT_LT(std::fabs(parallel_result - sequential_result), std::numeric_limits<double>::epsilon() * 100);
     }
 }
 
 TEST(Parallel_Operations_MPI, EXCETION_ON_ZERO_COUNT) {
-    EXPECT_THROW(integralParallel(5, 0, 0), std::runtime_error);
+    EXPECT_THROW(integralParallel(static_cast<double(*)(double)>(&cos), 5, 0, 0), std::runtime_error);
 }
 
 int main(int argc, char* argv[]) {
--- modules/task_1/zoreev_m_rectangle_method/rectangle_method.h
+++ modules/task_1/zoreev_m_rectangle_method/rectangle_method.h
@@ -5,10 +5,11 @@
 #include <mpi.h>
 
 #include <cmath>
+#include <functional>
 #include <stdexcept>
 
-double integralSeqential(double a, double b, size_t count);
+double integralSeqential(std::function<double(double)> integrable_function, double a, double b, size_t count);
 
-double integralParallel(double a, double b, size_t count);
+double integralParallel(std::function<double(double)> integrable_function, double a, double b, size_t count);
 
 #endif  // MODULES_TASK_1_ZOREEV_M_RECTANGLE_METHOD_RECTANGLE_METHOD_H_
