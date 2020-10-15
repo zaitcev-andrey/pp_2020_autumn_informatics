4c8db61c76e758ab48b2304fd965f8e817ae5b90
MaksimShagov <43129418+MaksimShagov@users.noreply.github.com>
2020-10-15 20:06:31 +0300

    fix formatting

--- modules/task_1/shagov_m_symbol_count/symbol_count.cpp
+++ modules/task_1/shagov_m_symbol_count/symbol_count.cpp
@@ -1,62 +1,58 @@
-// Copyright 2020 Shagov Maksim
+// Copyright 2020 Shagov Maksim  
 #include <mpi.h>
 #include <random>
 #include <ctime>
 #include <iostream>
-#include "symbol_count.h"
-
+#include "../../../modules/task1/shagov_m_symbol_count/symbol_count.h"
 
 std::string createRandomString(int sz) {
-	std::mt19937 gen;
-	gen.seed(static_cast<unsigned int>(time(0)));
-	std::string string(sz, NULL);
-	for (int i = 0; i < sz; i++) { string[i] = gen() % 100; }
-	return string;
+    std::mt19937 gen;
+    gen.seed(static_cast<unsigned int>(time(0)));
+    std::string string(sz, NULL);
+    for (int i = 0; i < sz; i++) { string[i] = gen() % 100; }
+    return string;
 }
 
 int calculateSymbolsCountSequental(const std::string& string, char symbol) {
-	if (string.size() == 0)
-		throw std::exception("Invalid size");
-	/*if (string[ == 0)
-		throw std::exception("Invalid size");*/
-	int count = 0;
-	for (int i = 0; i < string.size(); i++)
-		if (string[i] == symbol)
-			count++;
-	return count;
+    if (string.size() == 0)
+        throw std::exception("Invalid size");
+    int count = 0;
+    for (int i = 0; i < string.size(); i++)
+        if (string[i] == symbol)
+            count++;
+    return count;
 }
 
 int getParallelSymbolsCount(const std::string& global_string,
-							int count_size_vector, char symbol) {
-	if (global_string.size() == 0)
-		throw std::exception("Invalid size");
-	int size, rank;
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	std::cout << size;
-	std::cout << rank;
-	const int delta = count_size_vector / size;
-	int remainder = count_size_vector - delta * size;
+                            int count_size_vector, char symbol) {
+    if (global_string.size() == 0)
+        throw std::exception("Invalid size");
+    int size, rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    std::cout << size;
+    std::cout << rank;
+    const int delta = count_size_vector / size;
+    int remainder = count_size_vector - delta * size;
 
-	if (rank == 0) {
-		for (int proc = 1; proc < size; proc++) {
-			MPI_Send(&global_string[0] + remainder + proc * delta, delta,
-				MPI_INT, proc, 0, MPI_COMM_WORLD);
-		}
-	}
-	std::string local_string(delta, NULL);
-	if (rank == 0) {
-		local_string.resize(delta + remainder);
-		local_string = std::string(global_string.begin(),
-								   global_string.begin() + delta + remainder);
-	}
-	else {
-		MPI_Status status;
-		MPI_Recv(&local_string[0], delta, MPI_CHAR, 0, 0, MPI_COMM_WORLD, &status);
-	}
+    if (rank == 0) {
+        for (int proc = 1; proc < size; proc++) {
+            MPI_Send(&global_string[0] + remainder + proc * delta, delta,
+                MPI_INT, proc, 0, MPI_COMM_WORLD);
+        }
+    }
+    std::string local_string(delta, NULL);
+    if (rank == 0) {
+        local_string.resize(delta + remainder);
+        local_string = std::string(global_string.begin(),
+                                   global_string.begin() + delta + remainder);
+    } else {
+        MPI_Status status;
+        MPI_Recv(&local_string[0], delta, MPI_CHAR, 0, 0, MPI_COMM_WORLD, &status);
+    }
 
-	unsigned int global_sum = 0;
-	unsigned int local_sum = calculateSymbolsCountSequental(local_string, symbol);
-	MPI_Reduce(&local_sum, &global_sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-	return global_sum;
+    unsigned int global_sum = 0;
+    unsigned int local_sum = calculateSymbolsCountSequental(local_string, symbol);
+    MPI_Reduce(&local_sum, &global_sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    return global_sum;
 }
--- modules/task_1/shagov_m_symbol_count/symbol_count.h
+++ modules/task_1/shagov_m_symbol_count/symbol_count.h
@@ -1,4 +1,4 @@
-// Copyright 2020 Shagov Maksim
+// Copyright 2020 Shagov Maksim  
 #ifndef MODULES_TASK_1_SHAGOV_M_SYMBOL_COUNT_MPI_H_
 #define MODULES_TASK_1_SHAGOV_M_SYMBOL_COUNT_MPI_H_
 
@@ -7,5 +7,5 @@
 std::string createRandomString(int  sz);
 int calculateSymbolsCountSequental(const std::string& string, char symbol);
 int getParallelSymbolsCount(const std::string& global_string,
-							int count_size_vector, char symbol);
-#endif
+                            int count_size_vector, char symbol);
+#endif  // MODULES_TASK_1_SHAGOV_M_SYMBOL_COUNT_SYMBOL_COUNT_H_
