1106da73a5278d85c79df1144021ec28484d528a
Talesard <61109747+Talesard@users.noreply.github.com>
2020-10-17 23:06:21 +0300

    fix: type error

--- modules/task_1/napylov_e_different_characters/count_diff_char_mpi.cpp
+++ modules/task_1/napylov_e_different_characters/count_diff_char_mpi.cpp
@@ -20,7 +20,7 @@ char* getRandomString(int len) {
 
 int getSequentialDifferenceCount(const char *str1, const char *str2) {
     int diff_count = 0;
-    for (int i = 0; i < strlen(str1); i++) {
+    for (auto i = 0; i < strlen(str1); i++) {
         if (str1[i] != str2[i]) {
             diff_count++;
         }
