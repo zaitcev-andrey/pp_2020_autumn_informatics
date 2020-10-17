8d6220332faa10733e8faea492fcf75b4e29ce49
Talesard <61109747+Talesard@users.noreply.github.com>
2020-10-17 22:10:22 +0300

    first(daleko ne first)

---
+++ modules/task_1/napylov_e_different_characters/count_diff_char_mpi.h
@@ -0,0 +1,11 @@
+// Copyright 2020 Napylov Evgenii
+#ifndef MODULES_TASK_1_NAPYLOV_E_DIFFERENT_CHARACTERS_COUNT_DIFF_CHAR_MPI_H
+#define MODULES_TASK_1_NAPYLOV_E_DIFFERENT_CHARACTERS_COUNT_DIFF_CHAR_MPI_H
+
+
+char* getRandomString(int len);
+int getSequentialDifferenceCount(const char *str1, const char *str2);
+int getParallelDifferenceCount(const char *str1, const char *str2);
+
+
+#endif // MODULES_TASK_1_NAPYLOV_E_COUNTING_DIFFERENT_CHARACTERS_COUNT_DIFF_CHAR_MPI_H
