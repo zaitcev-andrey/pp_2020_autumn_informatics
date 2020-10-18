95005c203ed188ffdb1e0b0db7320703de79afc5
Diana Gruzdeva <taciturn.homebody@gmail.com>
2020-10-18 19:44:44 +0300

    Remove all tabs

--- modules/task_1/gruzdeva_d_trapezoidal_integral/main.cpp
+++ modules/task_1/gruzdeva_d_trapezoidal_integral/main.cpp
@@ -9,71 +9,71 @@ std::function<double(double)> composite = compositeFunction;
 
 
 TEST(Parallel_Operations_MPI, POLINOM_FROM_0_TO_1) {
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	double parallel_result = getParallelIntegral(0, 1, 10000, polinom);
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    double parallel_result = getParallelIntegral(0, 1, 10000, polinom);
 
-	if (rank == 0) {
-		double sequential_result = getSequentialIntegral(0, 1, 10000, polinom);
-		ASSERT_LT(std::fabs(parallel_result - sequential_result), 1.0e-8);
-	}
+    if (rank == 0) {
+        double sequential_result = getSequentialIntegral(0, 1, 10000, polinom);
+        ASSERT_LT(std::fabs(parallel_result - sequential_result), 1.0e-8);
+    }
 }
 
 TEST(Parallel_Operations_MPI, POLINOM_FROM_0_TO_10) {
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	double parallel_result = getParallelIntegral(0, 10, 10000, polinom);
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    double parallel_result = getParallelIntegral(0, 10, 10000, polinom);
 
-	if (rank == 0) {
-		double sequential_result = getSequentialIntegral(0, 10, 10000, polinom);
-		ASSERT_LT(std::fabs(parallel_result - sequential_result), 1.0e-4);
-	}
+    if (rank == 0) {
+        double sequential_result = getSequentialIntegral(0, 10, 10000, polinom);
+        ASSERT_LT(std::fabs(parallel_result - sequential_result), 1.0e-4);
+    }
 }
 
 TEST(Parallel_Operations_MPI, POLINOM_FROM_0_TO_1000) {
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	double parallel_result = getParallelIntegral(0, 1000, 10000, polinom);
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    double parallel_result = getParallelIntegral(0, 1000, 10000, polinom);
 
-	if (rank == 0) {
-		double sequential_result = getSequentialIntegral(0, 1000, 10000, polinom);
-		ASSERT_LT(std::fabs(parallel_result - sequential_result), 1.0e+4);
-	}
+    if (rank == 0) {
+        double sequential_result = getSequentialIntegral(0, 1000, 10000, polinom);
+        ASSERT_LT(std::fabs(parallel_result - sequential_result), 1.0e+4);
+    }
 }
 
 TEST(Parallel_Operations_MPI, COMPOSITE_FUNCTION_FROM_0_TO_1) {
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	double parallel_result = getParallelIntegral(0, 1, 10000, composite);
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    double parallel_result = getParallelIntegral(0, 1, 10000, composite);
 
-	if (rank == 0) {
-		double sequential_result = getSequentialIntegral(0, 1, 10000, composite);
-		ASSERT_LT(std::fabs(parallel_result - sequential_result), 1.0e-8);
-	}
+    if (rank == 0) {
+        double sequential_result = getSequentialIntegral(0, 1, 10000, composite);
+        ASSERT_LT(std::fabs(parallel_result - sequential_result), 1.0e-8);
+    }
 }
 
 TEST(Parallel_Operations_MPI, COMPOSITE_FUNCTION_FROM_0_TO_0_001) {
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	double parallel_result = getParallelIntegral(0, 0.001, 10000, composite);
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    double parallel_result = getParallelIntegral(0, 0.001, 10000, composite);
 
-	if (rank == 0) {
-		double sequential_result = getSequentialIntegral(0, 0.001, 10000, composite);
-		ASSERT_LT(std::fabs(parallel_result - sequential_result), 1.0e-17);
-	}
+    if (rank == 0) {
+        double sequential_result = getSequentialIntegral(0, 0.001, 10000, composite);
+        ASSERT_LT(std::fabs(parallel_result - sequential_result), 1.0e-17);
+    }
 }
 
 int main(int argc, char** argv) {
-	::testing::InitGoogleTest(&argc, argv);
-	MPI_Init(&argc, &argv);
+    ::testing::InitGoogleTest(&argc, argv);
+    MPI_Init(&argc, &argv);
 
-	::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
-	::testing::TestEventListeners& listeners =
-		::testing::UnitTest::GetInstance()->listeners();
+    ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
+    ::testing::TestEventListeners& listeners =
+        ::testing::UnitTest::GetInstance()->listeners();
 
-	listeners.Release(listeners.default_result_printer());
-	listeners.Release(listeners.default_xml_generator());
+    listeners.Release(listeners.default_result_printer());
+    listeners.Release(listeners.default_xml_generator());
 
-	listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
-	return RUN_ALL_TESTS();
+    listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
+    return RUN_ALL_TESTS();
 }
--- modules/task_1/gruzdeva_d_trapezoidal_integral/trapezoidal_integral.h
+++ modules/task_1/gruzdeva_d_trapezoidal_integral/trapezoidal_integral.h
@@ -13,4 +13,4 @@ double callFunction(std::function<double(double)> function, double value);
 double polinomFunction(double x);
 double compositeFunction(double x);
 
-#endif  MODULES_TASK_1_GRUZDEVA_D_TRAPEZOIDAL_INTEGRAL_TRAPEZOIDAL_INTEGRAL_H_
+#endif  // MODULES_TASK_1_GRUZDEVA_D_TRAPEZOIDAL_INTEGRAL_TRAPEZOIDAL_INTEGRAL_H_
