b39f3a016f7cf65761152723ffed152c69613429
MaksimShagov <43129418+MaksimShagov@users.noreply.github.com>
2020-10-18 19:21:05 +0300

    add empty test

--- modules/task_1/shagov_m_symbol_count/main.cpp
+++ modules/task_1/shagov_m_symbol_count/main.cpp
@@ -7,6 +7,17 @@
 #include <iostream>
 #include "./symbol_count.h"
 
+TEST(Parallel_Symbol_Count_MPI, Test_Empty_Symbols_String) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    const int count_size_string = 0;
+    std::string global_string;
+
+    if (rank == 0) {
+        ASSERT_ANY_THROW(getParallelSymbolsCount(global_string, count_size_string, 'A'));
+    }
+}
+
 TEST(Parallel_Symbol_Count_MPI, Test_10_Same_Symbols_String) {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
