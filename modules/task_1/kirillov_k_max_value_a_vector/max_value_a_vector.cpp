0271bf17462c6e8795d8aa86dc1fea2719744cc0
kustuk1306 <kustuk1306@gmail.com>
2020-10-18 18:58:02 +0300

    first

---
+++ modules/task_1/kirillov_k_max_value_a_vector/max_value_a_vector.cpp
@@ -0,0 +1,43 @@
+//Copyright 2020 Kirillov Konstantin
+#include<mpi.h>
+#include<vector>
+#include <ctime>
+#include<random>
+#include<algorithm>
+#include "../../../modules/task_1/kirillov_k_max_value_a_vector/max_value_a_vector.h"
+
+using namespace std;
+vector<int> getRandomVector(int size){
+    mt19937 gen;
+    gen.seed(static_cast<unsigned int>(time(0)));
+    vector<int>vec(size);
+    for(int i=0;i<size;i++){
+        vec[i]=gen()%100;
+    }
+    return vec;
+}
+int getSequentialMax(vector<int>vec){
+    return *max_element(vec.begin(),vec.end());
+}
+int getParallelMax(vector<int>vec,int size_vec){
+    int procNum,procRank;
+    MPI_Comm_size(MPI_COMM_WORLD,&procNum);
+    MPI_Comm_rank(MPI_COMM_WORLD,&procRank);
+    int delta=size_vec/procNum;
+    if(procRank==0){
+        for(int proc=1;proc<procNum;proc++){
+            MPI_Send(&vec[0]+proc*delta,delta,MPI_INT,proc,0,MPI_COMM_WORLD);
+        }
+    }
+    vector<int>local(delta);
+    if (procRank==0){
+        local=vector<int>(vec.begin(),vec.begin()+delta);
+    } else{
+        MPI_Status status;
+        MPI_Recv(&local[0],delta,MPI_INT,0,0,MPI_COMM_WORLD,&status);
+    }
+    int global_Max=0;
+    int local_Max=getSequentialMax(local);
+    MPI_Reduce(&local_Max,&global_Max,1,MPI_INT,MPI_MAX,0,MPI_COMM_WORLD);
+    return global_Max;
+}
---
+++ modules/task_1/kirillov_k_max_value_a_vector/max_value_a_vector.h
@@ -0,0 +1,11 @@
+//Copyright 2020 Kirillov Konstantin
+#ifndef MODULES_TASK_1_KIRIILOV_K_MAX_VALUE_A_VECTOR
+#define MODULES_TASK_1_KIRIILOV_K_MAX_VALUE_A_VECTOR
+#include <vector>
+using namespace std;
+
+vector<int> getRandomVector(int size);
+int getSequentialMax(vector<int>vec);
+int getParallelMax(vector<int>vec,int size_vec);
+
+#endif //MODULES_TASK_1_KIRIILOV_K_MAX_VALUE_A_VECTOR
