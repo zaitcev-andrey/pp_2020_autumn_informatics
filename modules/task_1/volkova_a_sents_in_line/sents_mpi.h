d2a0050384ab38d7a7f73e75c5f3486ab61c78c3
WolfCub15 <volkovanastyavolkova@gmail.com>
2020-10-19 11:00:21 +0300

    second

--- modules/task_1/volkova_a_sents_in_line/sents_mpi.h
+++ modules/task_1/volkova_a_sents_in_line/sents_mpi.h
@@ -9,4 +9,4 @@ int getParallelSumSentences(std::vector<char> global_line, int size_line);
 int getSequentialSumSentences(std::vector<char> line);
 
 
-#endif  // MODULES_TASK_1_VOLKOVA_A_SENTENCES_IN_LINE_SENTS_MPI_H_
+#endif  // MODULES_TASK_1_VOLKOVA_A_SENTS_IN_LINE_SENTS_MPI_H_
