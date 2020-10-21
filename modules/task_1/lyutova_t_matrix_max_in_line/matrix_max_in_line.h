13003a69ca05513b9605b1e1c3598e520ef6d6df
lutovatanya <lutovat20@gmail.com>
2020-10-21 17:32:28 +0300

    End

---
+++ modules/task_1/lyutova_t_matrix_max_in_line/matrix_max_in_line.h
@@ -0,0 +1,11 @@
+// Copyright 2020 Lyutova Tanya
+#ifndef MODULES_TASK_1_LYUTOVA_T_MATRIX_MAX_IN_LINE_MATRIX_MAX_IN_LINE_H_
+#define MODULES_TASK_1_LYUTOVA_T_MATRIX_MAX_IN_LINE_MATRIX_MAX_IN_LINE_H_
+
+#include <vector>
+
+std::vector<int> getRandomMatrix(int rows, int columns);
+std::vector<int> getMaxInLineSequental(const std::vector<int>matrix, int rows, int columns);
+std::vector<int> getMaxInLineParallel(const std::vector<int>matrix, int rows, int columns);
+
+#endif  // MODULES_TASK_1_LYUTOVA_T_MATRIX_MAX_IN_LINE_MATRIX_MAX_IN_LINE_H_
