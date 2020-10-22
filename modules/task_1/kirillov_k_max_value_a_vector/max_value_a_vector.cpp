c79356d11808f5a503b66551d6322d09f1041729
kustuk1306 <kustuk1306@gmail.com>
2020-10-22 12:17:58 +0300

    changes_second

--- modules/task_1/kirillov_k_max_value_a_vector/max_value_a_vector.cpp
+++ modules/task_1/kirillov_k_max_value_a_vector/max_value_a_vector.cpp
@@ -9,7 +9,7 @@
 std::vector<int> getRandomVector(int size) {
     std::mt19937 gen;
     gen.seed(static_cast<unsigned int>(time(0)));
-    std::vector<int> vec(size);
+    std::vector<int>vec(size);
     for (int i = 0; i < size; i++) {
         vec[i] = gen() % 100;
     }
