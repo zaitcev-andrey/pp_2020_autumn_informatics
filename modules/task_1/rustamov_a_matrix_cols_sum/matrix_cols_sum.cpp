ffb6e09b9f8fdd4618d1e23b589a19d27b28109c
RustamovAzer <43135865+RustamovAzer@users.noreply.github.com>
2020-10-22 12:41:19 +0300

    test 17

--- modules/task_1/rustamov_a_matrix_cols_sum/matrix_cols_sum.cpp
+++ modules/task_1/rustamov_a_matrix_cols_sum/matrix_cols_sum.cpp
@@ -38,13 +38,13 @@ Matrix ParallelColsSum(Matrix matrix, int rows, int cols) {
     int procNum, procRank;
     MPI_Comm_size(MPI_COMM_WORLD, &procNum);
     MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
-    if ((n == 0)&&(rows > 2)) {
+    if ((rows / procNum == 0)&&(rows > 2)) {
         const int n = 2;
         int procNumNew = rows / 2;
         int delta = rows % procNumNew;
     } else {
         int procNumNew = procNum;
-        const int n = rows / procNumNew;
+        const int n = New;
         int delta = rows % procNumNew;
     }
     Matrix global_sum(cols);
