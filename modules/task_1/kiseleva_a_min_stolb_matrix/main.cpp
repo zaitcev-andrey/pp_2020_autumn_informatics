244953037dd42ec460302ccbc5ed48f8af2f711a
netweel <netweel504@gmail.com>
2020-10-23 15:05:48 +0300

    first*

--- modules/task_1/kiseleva_a_min_stolb_matrix/main.cpp
+++ modules/task_1/kiseleva_a_min_stolb_matrix/main.cpp
@@ -7,13 +7,12 @@
 #include <iostream>
 using namespace std;
 
-TEST (Parallel_Operations_MPI, TEST_3x3)
+TEST (Parallel_MPI, TEST_3x3)
 {
 	int rank;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	int str = 3;
 	int stlb = 3;
-	int size = str * stlb;
 	vector<vector<int>> matrix = { {1,2,3},{0,0,0},{5,6,9} };
 	vector<int> RRes = { 0,0,0 };
 	vector<int> linm = VVconvertV(matrix,3,3);
@@ -21,57 +20,54 @@ TEST (Parallel_Operations_MPI, TEST_3x3)
 	res= Min(linm,3,3);
 	if (rank == 0)
 	{
-		for (int i = 0; i < columns; ++i)
+		for (int i = 0; i < stlb; ++i)
 			EXPECT_EQ(RRes[i],res[i]);
 	}
 }
 
-TEST (Parallel_Operations_MPI, TEST_2x3)
+TEST (Parallel_MPI, TEST_2x3)
 {
 	int rank;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	int str = 2;
 	int stlb = 3;
-	int size = str * stlb;
 	vector<vector<int>> matrix = { {0,5,9},{2,3,9} };
-	vector<int> RRes = { 0,3,9};
+	vector<int> RRes = {0,3,9};
 	vector<int> linm = VVconvertV(matrix, 2, 3);
 	vector<int> res(3);
 	res = Min(linm,2,3);
 	if (rank == 0)
 	{
-		for (int i = 0; i < columns; ++i)
+		for (int i = 0; i < stlb; ++i)
 			EXPECT_EQ(RRes[i], res[i]);
 	}
 }
 
 
-TEST (Parallel_Operations_MPI, TEST_OTR_EL)
+TEST (Parallel_MPI, TEST_OTR_EL)
 {
 	int rank;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	int str = 2;
 	int stlb = 2;
-	int size = str * stlb;
 	vector<vector<int>> matrix = { {-1,-9},{0,-50} };
-	vector<int> RRes = { -1,-50};
+	vector<int> RRes = {-1,-50};
 	vector<int> linm = VVconvertV(matrix, 2, 2);
 	vector<int> res(2);
 	res = Min(linm,2,2);
 	if (rank == 0)
 	{
-		for (int i = 0; i < columns; ++i)
+		for (int i = 0; i < stlb; ++i)
 			EXPECT_EQ(RRes[i], res[i]);
 	}
 }
 
-TEST(Parallel_Operations_MPI, TEST_RANDOM)
+TEST(Parallel_MPI, TEST_RANDOM)
 {
 	int rank;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	int str = 3;
 	int stlb = 3;
-	int size = str * stlb;
 	vector<vector<int>> matrix = { {0,0,0},{0,0,0},{0,0,0} };
 	vector<int> RRes = { 0,0,0 };
 	vector<int> linm = VVconvertV(matrix, 3, 3);
@@ -79,24 +75,24 @@ TEST(Parallel_Operations_MPI, TEST_RANDOM)
 	res = Min(linm, 3, 3);
 	if (rank == 0)
 	{
-		for (int i = 0; i < columns; ++i)
+		for (int i = 0; i < stlb; ++i)
 			EXPECT_EQ(RRes[i], res[i]);
 	}
 }
 
-TEST(Parallel_Operations_MPI, TEST_RANDOM_NOT_SQUARE)
+TEST(Parallel_MPI, TEST_RANDOM_NOT_SQUARE)
 {
 	int rank;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	int str = 3;
-	int stlb = 2;
+	int str = 2;
+	int stlb = 3;
 	int size = str * stlb;
-	vector<vector<int>> matrix(9);
-	matrix = RandomMatrix(3, 2);
-	vector<int> linm = VVconvertV(matrix, 3, 2);
-	vector<int> res = Min(linm, 3, 2);
-
+	vector<vector<int>> matrix(size);
+	matrix = RandomMatrix(2, 3);
+	vector<int> linm = VVconvertV(matrix, 2, 3);
+	vector<int> res = Min(linm, 2, 3);
 }
+
 
 
 
--- modules/task_1/kiseleva_a_min_stolb_matrix/min_stolb.cpp
+++ modules/task_1/kiseleva_a_min_stolb_matrix/min_stolb.cpp
@@ -9,7 +9,7 @@
 #include <iostream>
 using namespace std;
 
-vector<vector<int>> RandomMatrix( int str, int stlb) 
+vector<vector<int>> RandomMatrix(int str, int stlb)
 {
 	mt19937 gen;
 	gen.seed(static_cast<unsigned int>(time(0)));
@@ -17,11 +17,11 @@ vector<vector<int>> RandomMatrix( int str, int stlb)
 	vector<int> stolb(str);
 	for (int i = 0; i < stlb; i++)
 	{
-			stolb.clear();
-			for (int j = 0; j < str; j++)
-			{
-				stolb.push_back(gen() % 10);
-			}
+		stolb.clear();
+		for (int j = 0; j < str; j++)
+		{
+			stolb.push_back(gen() % 10);
+		}
 		matrix[i] = stolb;
 	}
 	return matrix;
@@ -40,25 +40,25 @@ vector<int> VVconvertV(vector<vector<int>> matrix, int str, int stlb)
 	return linm;
 }
 
-vector<int> Min(vector<int> linm, int str, int stlb) 
+vector<int> Min(vector<int> linm, int str, int stlb)
 {
 	int size, rank;
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-     int min = INT_MAX;
+	int min = INT_MAX;
 	vector<int> res(stlb);
 	int delta = stlb / size;
 	if (stlb % size != 0)
 	{
 		delta++;
 	}
-	MPI_Bcast(linm.data(), str * stlb, MPI_INT, 0, MPI_COMM_WORLD); 
-	  for (int i = delta * rank; i < delta * (rank + 1); i++)
-	  {
+	MPI_Bcast(linm.data(), str * stlb, MPI_INT, 0, MPI_COMM_WORLD);
+	for (int i = delta * rank; i < delta * (rank + 1); i++)
+	{
 		min = linm[i];
-		for (int j = 0; j < str; j++) 
+		for (int j = 0; j < str; j++)
 		{
-			if (linm[i + j * stlb] <= min) 
+			if (linm[i + j * stlb] <= min)
 			{
 				min = linm[i + j * stlb];
 			}
@@ -67,14 +67,14 @@ vector<int> Min(vector<int> linm, int str, int stlb)
 		{
 			res[i] = min;
 		}
-		else 
+		else
 		{
 			MPI_Send(&min, 1, MPI_INT, 0, 15, MPI_COMM_WORLD);
 		}
-	  }
+	}
 	if (rank == 0)
 	{
-		for (int i = 1; i < size; i++) 
+		for (int i = 1; i < size; i++)
 		{
 			for (int j = 0; j < delta; j++)
 			{
