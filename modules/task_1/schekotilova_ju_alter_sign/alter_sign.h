9842f586f37fcd35da63da7b5cae7ad1bc69a463
shekotilova-yuliya <shekotilova.yuliya@mai.ru>
2020-10-26 12:08:10 +0300

    task_1 fix2

--- modules/task_1/schekotilova_ju_alter_sign/alter_sign.h
+++ modules/task_1/schekotilova_ju_alter_sign/alter_sign.h
@@ -1,6 +1,6 @@
 // Copyright 2020 Schekotilova Julia
-#ifndef MODULES_TASK_1_SCHEKOTILOVA_JU_ALTER_SIGN_H_
-#define MODULES_TASK_1_SCHEKOTILOVA_JU_ALTER_SIGN_H_
+#ifndef MODULES_TASK_1_SCHEKOTILOVA_JU_ALTER_SIGN_ALTER_SIGN_H_
+#define MODULES_TASK_1_SCHEKOTILOVA_JU_ALTER_SIGN_ALTER_SIGN_H_
 
 #include <mpi.h>
 #include <vector>
@@ -12,4 +12,4 @@ std::vector<int> getRandomVector(int  sz);
 int getParallelOperations(std::vector<int> vec, int count_size_vec);
 int getSequentialOperations(std::vector<int> v, int size_v);
 
-#endif  // MODULES_TASK_1_SCHEKOTILOVA_JU_ALTER_SIGN_H_
+#endif  // MODULES_TASK_1_SCHEKOTILOVA_JU_ALTER_SIGN_ALTER_SIGN_H_
