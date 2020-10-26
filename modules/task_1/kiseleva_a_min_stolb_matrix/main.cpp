539eff173ad86aa4d8a22911f5f01167d9ea73e4
netweel <netweel504@gmail.com>
2020-10-27 01:01:46 +0300

    FINALLY

--- modules/task_1/kiseleva_a_min_stolb_matrix/main.cpp
+++ modules/task_1/kiseleva_a_min_stolb_matrix/main.cpp
@@ -51,19 +51,40 @@ TEST(Parallel_MPI, TEST_OTR_EL) {
     }
 }
 
-TEST(Parallel_MPI, TEST_RANDOM) {
+TEST(Parallel_MPI, TEST_RANDOM_7x4) {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    int stlb = 3;
-    std::vector<std::vector<int>> matrix = { {0, 0, 0}, {0, 0, 0}, {0, 0, 0} };
-    std::vector<int> RRes = { 0, 0, 0 };
-    std::vector<int> linm = VVconvertV(matrix, 3, 3);
-    std::vector<int> res(3);
-    res = Min(linm, 3, 3);
-    if (rank == 0) {
-        for (int i = 0; i < stlb; ++i)
-            EXPECT_EQ(RRes[i], res[i]);
-    }
+    int str = 7;
+    int stlb = 4;
+    int size = str * stlb;
+    std::vector<std::vector<int>> matrix(size);
+    matrix = RandomMatrix(7, 4);
+    std::vector<int> linm = VVconvertV(matrix, 7, 4);
+    std::vector<int> res = Min(linm, 7, 4);
+}
+
+TEST(Parallel_MPI, TEST_RANDOM_14x10) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    int str = 14;
+    int stlb = 10;
+    int size = str * stlb;
+    std::vector<std::vector<int>> matrix(size);
+    matrix = RandomMatrix(14, 10);
+    std::vector<int> linm = VVconvertV(matrix, 14, 10);
+    std::vector<int> res = Min(linm, 14, 10);
+}
+
+TEST(Parallel_MPI, TEST_RANDOM_4x7) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    int str = 4;
+    int stlb = 7;
+    int size = str * stlb;
+    std::vector<std::vector<int>> matrix(size);
+    matrix = RandomMatrix(4, 7);
+    std::vector<int> linm = VVconvertV(matrix, 4, 7);
+    std::vector<int> res = Min(linm, 4, 7);
 }
 
 TEST(Parallel_MPI, TEST_RANDOM_NOT_SQUARE) {
