e3c65aa518d08e55edb45d0ac64273078213d366
OlgaMakesThings <56171499+OlgaMakesThings@users.noreply.github.com>
2020-10-26 11:49:20 +0300

    new

---
+++ modules/task_1/panova_o_max_difference/max_difference.h
@@ -0,0 +1,9 @@
+//Copyright 2020 Panova Olga
+#ifndef MODULES_TASK_1_PANOVA_O_MAX_DIFFERENCE_H_
+#define MODULES_TASK_1_PANOVA_O_MAX_DIFFERENCE_H_
+#include <vector>
+using namespace std;
+vector<int> CreateVector(int size);
+int GetSequentialDifference(vector<int> v, int size);
+int GetParallelDifference(vector<int> vec, int size);
+#endif 
