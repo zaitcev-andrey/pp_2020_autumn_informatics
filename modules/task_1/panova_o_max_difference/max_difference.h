4af7e43fce90558fe5b190953d46a730e9706f13
OlgaMakesThings <56171499+OlgaMakesThings@users.noreply.github.com>
2020-10-26 13:01:36 +0300

    Update max_difference.h

--- modules/task_1/panova_o_max_difference/max_difference.h
+++ modules/task_1/panova_o_max_difference/max_difference.h
@@ -1,6 +1,6 @@
 // Copyright 2020 Panova Olga
 #ifndef MODULES_TASK_1_PANOVA_O_MAX_DIFFERENCE_MAX_DIFFERENCE_H_
-#define MODULES_TASK_1_PANOVA_O_MAX_DIFFERENCE_H_
+#define MODULES_TASK_1_PANOVA_O_MAX_DIFFERENCE_MAX_DIFFERENCE_H_
 #include <vector>
 std::vector<int> CreateVector(int size);
 int GetSequentialDifference(std::vector<int> v, int size);
