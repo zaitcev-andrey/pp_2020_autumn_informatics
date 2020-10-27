03d1cb280757c5989be37077e7e68638a8106233
delarinq <eks-pliz@ya.ru>
2020-10-27 21:12:35 +0300

    first

---
+++ modules/task_1/novozhilova_e_vector_sum/vector_sum.h
@@ -0,0 +1,10 @@
+#ifndef MODULES_TASK_1_NOVOZHILOVA_E_VECTOR_SUM_VECTOR_SUM_H
+#define MODULES_TASK_1_NOVOZHILOVA_E_VECTOR_SUM_VECTOR_SUM_H
+
+#include <vector>
+
+std::vector<int> GenVector(int size);
+int GetSeqSum(std::vector<int> seq_v);
+int GetParSum(std::vector<int> v, int v_size);
+
+#endif //MODULES_TASK_1_NOVOZHILOVA_E_VECTOR_SUM_VECTOR_SUM_H
