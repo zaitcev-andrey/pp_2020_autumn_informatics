0ae1a1fc26b2a532973734313503a79d9bdfdb0e
ParanichevaAlyona <43447194+ParanichevaAlyona@users.noreply.github.com>
2020-10-30 19:01:55 +0300

    Update min_in_rows.cpp

--- modules/task_1/paranicheva_a_min_in_rows/min_in_rows.cpp
+++ modules/task_1/paranicheva_a_min_in_rows/min_in_rows.cpp
@@ -5,7 +5,7 @@
 #include <random>
 #include <iostream>
 #include <vector>
-#include "../../../modules/task_1/belik_j_integration_trapez/IntegrationTrapez.h"
+#include "../../../modules/task_1/paranicheva_a_min_in_rows.h"
 
 std::vector<int> getRandomMatrix(int rows, int cols) {
     std::mt19937 gen;
