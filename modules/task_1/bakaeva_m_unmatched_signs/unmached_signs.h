3f6915f2f4732b09849c98db96ccd2890a6d8f2e
Maria Bakaeva <mary396969@gmail.com>
2020-10-30 13:50:30 +0300

    version10

--- modules/task_1/bakaeva_m_unmatched_signs/unmached_signs.h
+++ modules/task_1/bakaeva_m_unmatched_signs/unmached_signs.h
@@ -1,6 +1,6 @@
-//Copyright 2020 Bakaeva Maria
-#ifndef MODULES_TASK_1_BAKAEVA_M_UNMATCHED_SIGNS_H_
-#define MODULES_TASK_1_BAKAEVA_M_UNMATCHED_SIGNS_H_
+// Copyright 2020 Bakaeva Maria
+#ifndef MODULES_TASK_1_BAKAEVA_M_UNMATCHED_SIGNS_UNMACHED_SIGNS_H_
+#define MODULES_TASK_1_BAKAEVA_M_UNMATCHED_SIGNS_UNMACHED_SIGNS_H_
 
 
 char* generateString(int length);
@@ -8,5 +8,5 @@ int getSequentialUnmachedSignsCount(const char* str1, const char* str2);
 int getParallelUnmachedSignsCount(const char* str1_global,
     const char* str2_global);
 
-#endif //MODULES_TASK_1_BAKAEVA_M_UNMATCHED_SIGNS_H_
+#endif  // MODULES_TASK_1_BAKAEVA_M_UNMATCHED_SIGNS_UNMACHED_SIGNS_H_
 
