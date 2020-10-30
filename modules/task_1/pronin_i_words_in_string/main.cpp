02d847b2103cbb9a5ecf1729cb2514ecd71cb2d0
igor-pronin <gosha.pronin.2000@icloud.com>
2020-10-31 01:39:37 +0300

    fourth

--- modules/task_1/pronin_i_words_in_string/main.cpp
+++ modules/task_1/pronin_i_words_in_string/main.cpp
@@ -2,38 +2,33 @@
 #include <mpi.h>
 #include <gtest-mpi-listener.hpp> 
 #include <gtest/gtest.h> 
+#include <string>
 #include "./pronin_i_words_in_string.h"
 
-TEST(Parallel_Operations_MPI, Line_Size_101)
-{
+TEST(Parallel_Operations_MPI, Line_Size_101) {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     const int size = 101;
     std::string str = "";
-    if (rank == 0)
-    {
+    if (rank == 0) {
         str = RandomString(size);
     }
     int parallel = ParallelOperations(str, size);
-    if (rank == 0)
-    {
+    if (rank == 0) {
         int sequential = SequentialOperations(str);
         ASSERT_EQ(sequential, parallel);
     }
 }
-TEST(Parallel_Operations_MPI, Line_Size_200)
-{
+TEST(Parallel_Operations_MPI, Line_Size_200) {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     const int size = 200;
     std::string str = "";
-    if (rank == 0)
-    {
+    if (rank == 0) {
         str = RandomString(size);
     }
     int parallel = ParallelOperations(str, size);
-    if (rank == 0)
-    {
+    if (rank == 0) {
         int sequential = SequentialOperations(str);
         ASSERT_EQ(sequential, parallel);
     }
@@ -44,42 +39,35 @@ TEST(Parallel_Operations_MPI, Line_Size_1000)
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     const int size = 1000;
     std::string str = "";
-    if (rank == 0)
-    {
+    if (rank == 0) {
         str = RandomString(size);
     }
     int parallel = ParallelOperations(str, size);
-    if (rank == 0)
-    {
+    if (rank == 0) {
         int sequential = SequentialOperations(str);
         ASSERT_EQ(sequential, parallel);
     }
 }
-TEST(Parallel_Operations_MPI, Line_Size_10000)
-{
+TEST(Parallel_Operations_MPI, Line_Size_10000) {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     const int size = 10000;
     std::string str = "";
-    if (rank == 0)
-    {
+    if (rank == 0) {
         str = RandomString(size);
     }
     int parallel = ParallelOperations(str, size);
-    if (rank == 0)
-    {
+    if (rank == 0) {
         int sequential = SequentialOperations(str);
         ASSERT_EQ(sequential, parallel);
     }
 }
-TEST(Parallel_Operations_MPI, Own_string)
-{
+TEST(Parallel_Operations_MPI, Own_string) {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     std::string str = "Hello world";
     int parallel = ParallelOperations(str, str.size());
-    if (rank == 0)
-    {
+    if (rank == 0) {
         int sequential = SequentialOperations(str);
         ASSERT_EQ(sequential, parallel);
     }
--- modules/task_1/pronin_i_words_in_string/pronin_i_words_in_string.cpp
+++ modules/task_1/pronin_i_words_in_string/pronin_i_words_in_string.cpp
@@ -3,9 +3,9 @@
 #include <cstdlib>
 #include <ctime>
 #include <vector>
+#include <string>
 #include "../../../modules/task_1/pronin_i_words_in_string/pronin_i_words_in_string.h"
-std::string RandomString(int n)
-{
+std::string RandomString(int n) {
     if (n < 0)
         throw "Wrong size";
     srand(time(0));
@@ -16,34 +16,28 @@ std::string RandomString(int n)
         ss += s[rand() % strLen];
     return ss;
 }
-int SequentialOperations(std::string a)
-{
+int SequentialOperations(std::string a) {
     int count = 0;
-    for (int i = 0; i < a.size(); i++)
-    {
+    for (int i = 0; i < a.size(); i++) {
         if (a[i] == ' ')
             count++;
     }
     return (count + 1);
 }
-int ParallelOperations(std::string str, int len)
-{
+int ParallelOperations(std::string str, int len) {
     int size, rank;
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     const int delta = len / size;
     const int epsilon = len - delta * size;
-    if (rank == 0)
-    {
+    if (rank == 0) {
         for (int i = 1; i < size; i++)
             MPI_Send(str.data() + epsilon + delta * i, delta, MPI_CHAR, i, 0, MPI_COMM_WORLD);
     }
     std::string localStr = "";
-    if (rank == 0)
-    {
+    if (rank == 0) {
         localStr = std::string(str.cbegin(), str.cbegin() + epsilon + delta);
-    }
-    else {
+    } else {
         std::vector <char> local(delta);
         MPI_Status status;
         MPI_Recv(&local[0], delta, MPI_CHAR, 0, 0, MPI_COMM_WORLD, &status);
