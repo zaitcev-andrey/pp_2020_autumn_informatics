665256ce1da8cf5661acae8a0fcab1cebd5a9f6c
igor-pronin <gosha.pronin.2000@icloud.com>
2020-10-31 01:17:05 +0300

    third

--- modules/task_1/pronin_i_words_in_string/pronin_i_words_in_string.h
+++ modules/task_1/pronin_i_words_in_string/pronin_i_words_in_string.h
@@ -2,9 +2,8 @@
 #ifndef MODULES_TASK_1_PRONIN_I_WORDS_IN_STRING_WORDS_IN_STRING_H_
 #define MODULES_TASK_1_PRONIN_I_WORDS_IN_STRING_WORDS_IN_STRING_H_
 #include <string>
-using namespace std;
-string RandomString(int);
-int SequentialOperations(string);
-int ParallelOperations(string, int);
+std::string RandomString(int);
+int SequentialOperations(std::string);
+int ParallelOperations(std::string, int);
 
 #endif  // MODULES_TASK_1_PRONIN_I_WORDS_IN_STRING_WORDS_IN_STRING_H_
