9e6d8c20e02347065eb71c2242ce67ce7e862e51
lafas155007 <lafas155007@mail.ru>
2020-10-31 13:56:29 +0300

    Added lab1

---
+++ modules/task_1/zaikin_i_montecarlo/montecarlo.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef MODULES_TASK_1_ZAIKIN_I_MONTECARLO_MONTECARLO_H_
+#define MODULES_TASK_1_ZAIKIN_I_MONTECARLO_MONTECARLO_H_
+
+#include <mpi.h>
+#include <math.h>
+#include <iostream>
+
+
+float MonteCarloNotParallelMethod(double b, double a, int n, double(*func)(double));
+float MonteCarloParallelMethod(double b, double a, int n, double(*func)(double));
+
+
+#endif
