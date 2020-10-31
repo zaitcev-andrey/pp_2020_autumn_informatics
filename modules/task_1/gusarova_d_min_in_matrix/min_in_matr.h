b8b96175a858424afd1de8b39e29cf7027997cbc
DariaGusarova8192 <dashagysarova@gmail.com>
2020-10-31 21:28:57 +0300

    first

---
+++ modules/task_1/gusarova_d_min_in_matrix/min_in_matr.h
@@ -0,0 +1,9 @@
+#ifndef MODULES_TASK_1_GUSAROVA_D_MIN_IN_MATRIX_MIN_IN_MATR_H
+#define MODULES_TASK_1_GUSAROVA_D_MIN_IN_MATRIX_MIN_IN_MATR_H
+
+std::vector<std::vector<int>> RandMatr(int str, int st);
+std::vector<int> MatrInArr(std::vector<std::vector<int>>, int str, int st);
+int MinInArr(std::vector<int> A);
+int ParMin(std::vector<int> A);
+
+#endif MODULES_TASK_1_GUSAROVA_D_MIN_IN_MATRIX_MIN_IN_MATR_H
