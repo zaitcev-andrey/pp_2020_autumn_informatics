f15d3167c55c92c4b206fafa994b17f06838782f
Leroyku <mark.kumbrasev@gmail.com>
2020-11-01 19:37:55 +0300

    fix7

--- modules/task_1/kumbrasev_m_diff_char_counter/diff_char_counter.cpp
+++ modules/task_1/kumbrasev_m_diff_char_counter/diff_char_counter.cpp
@@ -18,7 +18,7 @@ std::string get_random_string() {
 std::size_t difference_count(seq_policy, const std::string& str_lhs, const std::string& str_rhs) {
     std::size_t counter{};
     for (std::size_t i = 0; i < str_lhs.size(); i++) {
-        if (str_lhs[i] != str_rhs[i]){
+        if (str_lhs[i] != str_rhs[i]) {
         counter++;
         }
     }
