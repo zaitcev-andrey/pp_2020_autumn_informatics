86c6d344a65f2b1ee1842d62727b38475c0daa24
ini <ikrini@list.ru>
2020-11-03 23:13:52 +0300

    Сумма значений по столбцам матрицы.

---
+++ modules/task_1/ikromov_i_sum_col/sum_col.h
@@ -0,0 +1,11 @@
+// Copyright 2020 Ikromov Inom
+#ifndef MODULES_TASK_1_IKROMOV_I_SUM_COL_SUM_COL_H_
+#define MODULES_TASK_1_IKROMOV_I_SUM_COL_SUM_COL_H_
+#include <vector>
+
+std::vector<std::vector<int>> getRandomMatrix(const int rows,
+    const int cols);
+std::vector<int> summColumnsOneProc(std::vector<std::vector<int>>);
+std::vector<int> summColumns(std::vector<std::vector<int>>);
+
+#endif  // MODULES_TASK_1_IKROMOV_I_SUM_COL_SUM_COL_H_
