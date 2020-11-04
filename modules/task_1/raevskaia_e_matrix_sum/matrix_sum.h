05a09e532b5109939a604bf56aaea753a2633880
Cathrine200000 <kkatya200000@gmail.com>
2020-11-04 17:29:32 +0300

    Fix

--- modules/task_1/raevskaia_e_matrix_sum/matrix_sum.h
+++ modules/task_1/raevskaia_e_matrix_sum/matrix_sum.h
@@ -8,4 +8,4 @@ std::vector<int> getRandomMatrix(int n, int m);
 int getParallelSum(std::vector<int> matrix, int matrix_size_n, int matrix_size_m);
 int getSequentialSum(std::vector<int> vec);
 
-#endif  // MODULES TASK_1_RAEVSKAIA_E_MATRIX_SUM_MATRIX_SUM_H_
+#endif  // MODULES_TASK_1_RAEVSKAIA_E_MATRIX_SUM_MATRIX_SUM_H_
