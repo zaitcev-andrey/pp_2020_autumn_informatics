dbb800b17b0a636562a226d5a9409bf6c5c6720e
Sergey Shirokov <43002246+sh1rokovs@users.noreply.github.com>
2020-11-05 16:27:08 +0300

    fix_2

--- modules/task_1/shirokov_s_matrix_min/min_elem_matrix.cpp
+++ modules/task_1/shirokov_s_matrix_min/min_elem_matrix.cpp
@@ -7,6 +7,52 @@
 #include "./min_elem_matrix.h"
 #include "../../../modules/task_1/shirokov_s_matrix_min/min_elem_matrix.h"
 
+void matrix_enter(int sizel_f, int sizec_f, int* a[]) {
+    std::random_device rd;
+    std::mt19937 number(rd());
+    for (int i = 0; i < sizel_f; i++) {
+        for (int j = 0; j < sizec_f; j++) {
+            a[i][j] = static_cast<int>(number() % 100u);
+        }
+    }
+}
+void enter_massive(int sizel_f, int* a) {
+    for (int i = 0; i < sizel_f; i++) {
+        a[i] = 0;
+        std::cout << a[i] << " ";
+    }
+}
+void recurv_massive(int sizel_f, int* a) {
+    for (int i = 0; i < sizel_f; i++) {
+        a[i]++;
+        std::cout << a[i] << " ";
+    }
+}
+void matrix_print(int sizel_f, int sizec_f, int* a[]) {
+    for (int i = 0; i < sizel_f; i++) {
+        for (int j = 0; j < sizec_f; j++) {
+            std::cout << a[i][j] << " ";
+            if ((a[i][j] / 10) == 0)
+                std::cout << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+void massive_ent(int sizel_f, int sizec_f, int* a[], int a1[]) {
+    for (int i = 0; i < sizec_f; i++) {
+        a1[i] = a[sizel_f][i];
+    }
+}
+int min_el(int sizec_f, int a[]) {
+    int min = 0;
+    for (int i = 0; i < sizec_f; i++) {
+        if (i == 0)
+            min = a[i];
+        if (a[i] < min)
+            min = a[i];
+    }
+    return min;
+}
 int maxElementMatrix(int sizel_f, int sizec_f) {
     int ProcNum, ProcRank, min = 0;
     MPI_Status status;
--- modules/task_1/shirokov_s_matrix_min/min_elem_matrix.h
+++ modules/task_1/shirokov_s_matrix_min/min_elem_matrix.h
@@ -5,52 +5,12 @@
 #include <iostream>
 #include <vector>
 
-void matrix_enter(int sizel_f, int sizec_f, int* a[]) {
-    std::random_device rd;
-    std::mt19937 number(rd());
-    for (int i = 0; i < sizel_f; i++) {
-        for (int j = 0; j < sizec_f; j++) {
-            a[i][j] = static_cast<int>(number() % 100u);
-        }
-    }
-}
-void enter_massive(int sizel_f, int* a) {
-    for (int i = 0; i < sizel_f; i++) {
-        a[i] = 0;
-        std::cout << a[i] << " ";
-    }
-}
-void recurv_massive(int sizel_f, int* a) {
-    for (int i = 0; i < sizel_f; i++) {
-        a[i]++;
-        std::cout << a[i] << " ";
-    }
-}
-void matrix_print(int sizel_f, int sizec_f, int* a[]) {
-    for (int i = 0; i < sizel_f; i++) {
-        for (int j = 0; j < sizec_f; j++) {
-            std::cout << a[i][j] << " ";
-            if ((a[i][j] / 10) == 0)
-                std::cout << " ";
-        }
-        std::cout << std::endl;
-    }
-}
-void massive_ent(int sizel_f, int sizec_f, int* a[], int a1[]) {
-    for (int i = 0; i < sizec_f; i++) {
-        a1[i] = a[sizel_f][i];
-    }
-}
-int min_el(int sizec_f, int a[]) {
-    int min = 0;
-    for (int i = 0; i < sizec_f; i++) {
-        if (i == 0)
-            min = a[i];
-        if (a[i] < min)
-            min = a[i];
-    }
-    return min;
-}
+void matrix_enter(int sizel_f, int sizec_f, int* a[]);
+void enter_massive(int sizel_f, int* a);
+void recurv_massive(int sizel_f, int* a);
+void matrix_print(int sizel_f, int sizec_f, int* a[]);
+void massive_ent(int sizel_f, int sizec_f, int* a[], int a1[]);
+int min_el(int sizec_f, int a[]);
 int maxElementMatrix(int sizel_f, int sizec_f);
 
 #endif  // MODULES_TASK_1_SHIROKOV_S_MATRIX_MIN_MIN_ELEM_MATRIX_H_
