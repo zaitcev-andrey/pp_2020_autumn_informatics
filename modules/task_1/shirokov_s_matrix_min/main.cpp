f26c83dc2925a52070dd9215ba5594d7086bb212
Sergey Shirokov <43002246+sh1rokovs@users.noreply.github.com>
2020-11-05 17:06:15 +0300

    fix_4

--- modules/task_1/shirokov_s_matrix_min/main.cpp
+++ modules/task_1/shirokov_s_matrix_min/main.cpp
@@ -7,9 +7,9 @@
 TEST(Parallel_Matrix_Max_MPI, Size_0x0) {
     int procRank;
     MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
-    int sizel = 0, sizec = 0, max;
+    int sizel = 0, sizec = 0;
     if (procRank == 0) {
-        ASSERT_ANY_THROW(max = maxElementMatrix(sizel, sizec));
+        ASSERT_ANY_THROW(maxElementMatrix(sizel, sizec));
     }
 }
 
