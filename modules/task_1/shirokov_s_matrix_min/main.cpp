058b1f825abe70ddd16f689072e673101ab0a6f8
Sergey Shirokov <43002246+sh1rokovs@users.noreply.github.com>
2020-11-05 17:24:10 +0300

    fix_4

--- modules/task_1/shirokov_s_matrix_min/main.cpp
+++ modules/task_1/shirokov_s_matrix_min/main.cpp
@@ -4,31 +4,12 @@
 #include <gtest-mpi-listener.hpp>
 #include "./min_elem_matrix.h"
 
-TEST(Parallel_Matrix_Max_MPI, Size_0x0) {
-    int procRank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
-    int sizel = 0, sizec = 0;
-    if (procRank == 0) {
-        ASSERT_ANY_THROW(maxElementMatrix(sizel, sizec));
-    }
-}
-
 TEST(Parallel_Matrix_Max_MPI, Size_100x100) {
     int procRank;
     MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
     int sizel = 100, sizec = 100;
     if (procRank == 0) {
         ASSERT_TRUE(sizel == 100 && sizec == 100);
-    }
-}
-
-TEST(Parallel_Matrix_Max_MPI, Size_ProcNum) {
-    int procRank, procNum;
-    MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
-    MPI_Comm_size(MPI_COMM_WORLD, &procNum);
-    int sizel = 10;
-    if (procRank == 0) {
-        ASSERT_TRUE(procNum == sizel || sizel < procNum);
     }
 }
 
