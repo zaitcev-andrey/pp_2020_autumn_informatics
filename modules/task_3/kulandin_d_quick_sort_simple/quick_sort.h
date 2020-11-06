8f37bdbf9e756debb1262d2ad4dca1e183565580
Denis Kulandin <kulandin08@gmail.com>
2020-11-06 17:50:58 +0300

    draft quick sort

---
+++ modules/task_3/kulandin_d_quick_sort_simple/quick_sort.h
@@ -0,0 +1,7 @@
+// Copyright 2020 Kulandin Denis
+#include <vector>
+
+std::vector<int> randomVector(int sz);
+void division(std::vector<int> &a, int l, int r, int &t);
+void quickSort(std::vector<int> & a, int l, int r);
+std::vector<int> parallelQuickSort(const std::vector<int> &a);
