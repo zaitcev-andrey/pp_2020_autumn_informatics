bb50eb65961607f23f37ca943d61bc38c9577cb5
Elizaveta Prokofeva <prokof.15@mail.ru>
2020-11-15 19:46:11 +0300

    fix2

--- modules/task_2/prokofeva_e_linear_array/linear_array.cpp
+++ modules/task_2/prokofeva_e_linear_array/linear_array.cpp
@@ -11,23 +11,27 @@ int* getRandomVector(int size) {
 	std::mt19937 random_generator;
 	random_generator.seed(static_cast<unsigned int>(time(0)));
 	int* vec = new int[size];
-	for (int i = 0; i < size; i++) { vec[i] = random_generator() % 100; }
+	for (int i = 0; i < size; i++) {
+		vec[i] = random_generator() % 100;
+	}
 	return vec;
 }
 
 MPI_Comm create_comm(MPI_Comm comm) {
 	int size;
-	
+
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm graphcomm;
+
 	int* index = new int[size];
 	int* edges = new int[2 * (size - 1)];
 	int reorder = 0;
 	int k = 1;
 
 	index[0] = 1;
-	for (int i = 1; i < size - 1; i++)
+	for (int i = 1; i < size - 1; i++) {
 		index[i] = index[i - 1] + 2;
+	}
 	index[size - 1] = index[size - 2] + 1;
 
 	edges[0] = 1;
@@ -73,9 +77,8 @@ int* send_recv(int* buf, int count, MPI_Datatype type, int dest, int tag, MPI_Co
 				} else {
 					MPI_Send(&local_buf[0], count, type, neighbors[1], 0, comm);
 				}
-			}
-			else if (ProcRank == proc + 1) {
-				if (ProcRank == size - 1) {
+			} else if (ProcRank == proc + 1) {
+				if (ProcRank == proc + 1) {
 					MPI_Recv(&local_buf[0], count, type, neighbors[0], 0, comm, &status);
 				} else {
 					MPI_Recv(&local_buf[0], count, type, neighbors[0], 0, comm, &status);
@@ -101,6 +104,5 @@ int* send_recv(int* buf, int count, MPI_Datatype type, int dest, int tag, MPI_Co
 			}
 		}
 	}
-
 	return local_buf;
 }
