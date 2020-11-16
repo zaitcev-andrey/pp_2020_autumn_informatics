0d5a145f73e6e58a3e70fc758ff44cc9013cb68c
Nikita Kuznetsov <nikita0bond@gmail.com>
2020-11-16 11:15:25 +0300

    Add firts test

--- modules/task_2/kuznetsov_gaussian_vertical/main.cpp
+++ modules/task_2/kuznetsov_gaussian_vertical/main.cpp
@@ -4,18 +4,27 @@
 #include <vector>
 #include "./gaussian_vertical.h"
 
-
+TEST(GAUSSIAN_STRIP_VERTICAL_SCHEME, THROWS_WHEN_OVERAL_SIZE_LOWER_THAN_ZERO) {
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  std::vector<double> mat;
+  const int size_x = -10;
+  const int size_y = -10;
+  if (rank == 0) {
+    ASSERT_ANY_THROW(mat = getMatrix(size_x, size_y));
+  }
+}
 
 int main(int argc, char** argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    MPI_Init(&argc, &argv);
+  ::testing::InitGoogleTest(&argc, argv);
+  MPI_Init(&argc, &argv);
 
-    ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
-    ::testing::TestEventListeners& listeners = ::testing::UnitTest::GetInstance()->listeners();
+  ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
+  ::testing::TestEventListeners& listeners = ::testing::UnitTest::GetInstance()->listeners();
 
-    listeners.Release(listeners.default_result_printer());
-    listeners.Release(listeners.default_xml_generator());
+  listeners.Release(listeners.default_result_printer());
+  listeners.Release(listeners.default_xml_generator());
 
-    listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
-    return RUN_ALL_TESTS();
+  listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
+  return RUN_ALL_TESTS();
 }
