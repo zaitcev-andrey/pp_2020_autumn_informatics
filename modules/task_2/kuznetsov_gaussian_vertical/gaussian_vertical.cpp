23f363e40fb5309e31d32d122b57985c7429ac03
Nikita Kuznetsov <nikita0bond@gmail.com>
2020-11-16 10:39:25 +0300

    first commit

---
+++ modules/task_2/kuznetsov_gaussian_vertical/gaussian_vertical.cpp
@@ -0,0 +1,9 @@
+// Copyright 2020 Kuznetsov Nikita
+#include <mpi.h>
+#include <vector>
+#include <ctime>
+#include <random>
+#include <utility>
+#include "../../../modules/task_2/kuznetsov_gaussian_vertical/gaussian_vertical.h"
+
+
---
+++ modules/task_2/kuznetsov_gaussian_vertical/main.cpp
@@ -0,0 +1,21 @@
+// Copyright 2020 Kuznetsov Nikita
+#include <gtest-mpi-listener.hpp>
+#include <gtest/gtest.h>
+#include <vector>
+#include "./gaussian_vertical.h"
+
+
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    MPI_Init(&argc, &argv);
+
+    ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
+    ::testing::TestEventListeners& listeners = ::testing::UnitTest::GetInstance()->listeners();
+
+    listeners.Release(listeners.default_result_printer());
+    listeners.Release(listeners.default_xml_generator());
+
+    listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
+    return RUN_ALL_TESTS();
+}
