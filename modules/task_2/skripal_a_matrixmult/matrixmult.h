dd4bd7baeef24f954f4ee054a03ad1781e4f70b7
Skripal Andrey <askripal2@mail.ru>
2020-11-19 17:22:35 +0300

    second

--- modules/task_2/skripal_a_matrixmult/matrixmult.h
+++ modules/task_2/skripal_a_matrixmult/matrixmult.h
@@ -1,6 +1,6 @@
 // Copyright 2020 Skripal Andrey
-#ifndef MODULES_TASK_2_SKRIPAL_A_MATRIX_MULT_MATRIXMULT_H_
-#define MODULES_TASK_2_SKRIPAL_A_MATRIX_MULT_MATRIXMULT_H_
+#ifndef MODULES_TASK_2_SKRIPAL_A_MATRIXMULT_MATRIXMULT_H_
+#define MODULES_TASK_2_SKRIPAL_A_MATRIXMULT_MATRIXMULT_H_
 #include <vector>
 
 std::vector<double> transposition(std::vector<double> B, int size);
@@ -8,4 +8,4 @@ std::vector<double> sequentialMatrixMult(std::vector<double> A, std::vector<doub
 std::vector<double> genMatrix(int size);
 std::vector<double> parallelMatrixMult(const std::vector<double> &A, const std::vector<double> &B, int size);
 
-#endif  //  MODULES_TASK_2_SKRIPAL_A_MATRIX_MULT_MATRIXMULT_H_
+#endif  //  MODULES_TASK_2_SKRIPAL_A_MATRIXMULT_MATRIXMULT_H_
