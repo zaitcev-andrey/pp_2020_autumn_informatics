a2fbec01b8929da32ac06b1e639da8c54bfa33c7
Andrew <andrey.aleksandrychev2@mail.ru>
2020-11-19 14:09:19 +0300

    Second

--- modules/task_2/aleksandrychev_a_my_reduce/main.cpp
+++ modules/task_2/aleksandrychev_a_my_reduce/main.cpp
@@ -11,14 +11,14 @@ TEST(Parallel_Count_Sentences_MPI, summ_of_INT_size) {
     int size, rank;
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    
-	int root = 0;
-	int official_Reduce, my_Reduce = 0;
-	int local_value = (rank + 1) * 3;
-	
-	MPI_Reduce(&local_value, &official_Reduce, 1, MPI_INT, MPI_SUM, root, MPI_COMM_WORLD);
-	my_reduce(&local_value, &my_Reduce, 1, MPI_INT, MPI_SUM, root, MPI_COMM_WORLD);
-	
+
+    int root = 0;
+    int official_Reduce, my_Reduce = 0;
+    int local_value = (rank + 1) * 3;
+
+    MPI_Reduce(&local_value, &official_Reduce, 1, MPI_INT, MPI_SUM, root, MPI_COMM_WORLD);
+    my_reduce(&local_value, &my_Reduce, 1, MPI_INT, MPI_SUM, root, MPI_COMM_WORLD);
+
     if (rank == root) {
         ASSERT_EQ(official_Reduce, official_Reduce);
     }
@@ -28,14 +28,14 @@ TEST(Parallel_Count_Sentences_MPI, max_of_INT_size) {
     int size, rank;
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    
-	int root = 0;
-	int official_Reduce, my_Reduce = -2147483647;
-	int local_value = (rank + 1) * 3;
-	
-	MPI_Reduce(&local_value, &official_Reduce, 1, MPI_INT, MPI_MAX, root, MPI_COMM_WORLD);
-	my_reduce(&local_value, &my_Reduce, 1, MPI_INT, MPI_MAX, root, MPI_COMM_WORLD);
-	
+
+    int root = 0;
+    int official_Reduce, my_Reduce = -2147483647;
+    int local_value = (rank + 1) * 3;
+
+    MPI_Reduce(&local_value, &official_Reduce, 1, MPI_INT, MPI_MAX, root, MPI_COMM_WORLD);
+    my_reduce(&local_value, &my_Reduce, 1, MPI_INT, MPI_MAX, root, MPI_COMM_WORLD);
+
     if (rank == root) {
         ASSERT_EQ(official_Reduce, official_Reduce);
     }
@@ -45,14 +45,14 @@ TEST(Parallel_Count_Sentences_MPI, min_of_INT_size) {
     int size, rank;
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    
-	int root = 0;
-	int official_Reduce, my_Reduce = 2147483647;
-	int local_value = (rank + 1) * 3;
-	
-	MPI_Reduce(&local_value, &official_Reduce, 1, MPI_INT, MPI_MIN, root, MPI_COMM_WORLD);
-	my_reduce(&local_value, &my_Reduce, 1, MPI_INT, MPI_MIN, root, MPI_COMM_WORLD);
-	
+
+    int root = 0;
+    int official_Reduce, my_Reduce = 2147483647;
+    int local_value = (rank + 1) * 3;
+
+    MPI_Reduce(&local_value, &official_Reduce, 1, MPI_INT, MPI_MIN, root, MPI_COMM_WORLD);
+    my_reduce(&local_value, &my_Reduce, 1, MPI_INT, MPI_MIN, root, MPI_COMM_WORLD);
+
     if (rank == root) {
         ASSERT_EQ(official_Reduce, official_Reduce);
     }
@@ -62,14 +62,14 @@ TEST(Parallel_Count_Sentences_MPI, prod_of_INT_size) {
     int size, rank;
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    
-	int root = 0;
-	int official_Reduce, my_Reduce = 1;
-	int local_value = (rank + 1) * 3;
-	
-	MPI_Reduce(&local_value, &official_Reduce, 1, MPI_INT, MPI_PROD, root, MPI_COMM_WORLD);
-	my_reduce(&local_value, &my_Reduce, 1, MPI_INT, MPI_PROD, root, MPI_COMM_WORLD);
-	
+
+    int root = 0;
+    int official_Reduce, my_Reduce = 1;
+    int local_value = (rank + 1) * 3;
+
+    MPI_Reduce(&local_value, &official_Reduce, 1, MPI_INT, MPI_PROD, root, MPI_COMM_WORLD);
+    my_reduce(&local_value, &my_Reduce, 1, MPI_INT, MPI_PROD, root, MPI_COMM_WORLD);
+
     if (rank == root) {
         ASSERT_EQ(official_Reduce, official_Reduce);
     }
@@ -79,29 +79,28 @@ TEST(Parallel_Count_Sentences_MPI, summ_of_INT_list) {
     int size, rank;
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	
-	int count = 2;
-    
-	int root = 0;
-	int* official_Reduce = new int[count];
-	int* my_Reduce = new int[count];
-	for (int i = 0; i < count; i++){
-		my_Reduce[i] = 0;
-	}
-	
-	int* local_value = new int[count];
-	for (int i = 0; i < count; i++) {
-		local_value[i] = (rank + 1) * 3 * (i + 1);
-	}
-	
-	MPI_Reduce(local_value, official_Reduce, count, MPI_INT, MPI_SUM, root, MPI_COMM_WORLD);
-	my_reduce(local_value, my_Reduce, count, MPI_INT, MPI_SUM, root, MPI_COMM_WORLD);
+
+    int count = 2;
+
+    int root = 0;
+    int* official_Reduce = new int[count];
+    int* my_Reduce = new int[count];
+    for (int i = 0; i < count; i++) {
+        my_Reduce[i] = 0;
+    }
+
+    int* local_value = new int[count];
+    for (int i = 0; i < count; i++) {
+        local_value[i] = (rank + 1) * 3 * (i + 1);
+    }
+
+    MPI_Reduce(local_value, official_Reduce, count, MPI_INT, MPI_SUM, root, MPI_COMM_WORLD);
+    my_reduce(local_value, my_Reduce, count, MPI_INT, MPI_SUM, root, MPI_COMM_WORLD);
 
     if (rank == root) {
-		for (int i = 0; i < count; i++){
-			ASSERT_EQ(official_Reduce[i], my_Reduce[i]);
-		}
-        
+        for (int i = 0; i < count; i++) {
+            ASSERT_EQ(official_Reduce[i], my_Reduce[i]);
+        }
     }
 }
 
--- modules/task_2/aleksandrychev_a_my_reduce/my_reduce.cpp
+++ modules/task_2/aleksandrychev_a_my_reduce/my_reduce.cpp
@@ -18,7 +18,7 @@ int my_reduce(void* sendbuf, void* recvbuf, int count, MPI_Datatype datatype, MP
     int size, rank;
     MPI_Comm_size(comm, &size);
     MPI_Comm_rank(comm, &rank);
-	
+
     void* buffer;
 
     if (datatype == MPI_INT) {
@@ -30,143 +30,143 @@ int my_reduce(void* sendbuf, void* recvbuf, int count, MPI_Datatype datatype, MP
     } else {
         return MPI_ERR_TYPE;
     }
-		
-	if (rank != root) {
-		MPI_Send(sendbuf, count, datatype, root, 0, comm);
-	}
-		
-	if (rank == root) {
-		for (int proc = 0; proc < size; proc++) {
-			if (proc == root) {
-				if (datatype == MPI_INT) {
-					if (op == MPI_MAX) {
-						for (int j = 0; j < count; ++j) {
-							if (static_cast<int *>(recvbuf)[j] < static_cast<int *>(sendbuf)[j])
-								static_cast<int *>(recvbuf)[j] = static_cast<int *>(sendbuf)[j];
-						}
-					} else if (op == MPI_MIN) {
-						for (int j = 0; j < count; ++j) {
-							if (static_cast<int *>(recvbuf)[j] > static_cast<int *>(sendbuf)[j])
-								static_cast<int *>(recvbuf)[j] = static_cast<int *>(sendbuf)[j];
-						}
-					} else if (op == MPI_SUM) {
-						for (int j = 0; j < count; ++j) {
-							static_cast<int*>(recvbuf)[j] += static_cast<int*>(sendbuf)[j];
-						}
-					} else if (op == MPI_PROD) {
-						for (int j = 0; j < count; ++j) {
-							static_cast<int *>(recvbuf)[j] *= static_cast<int *>(sendbuf)[j];
-						}
-					}
-				} else if (datatype == MPI_FLOAT) {
-					if (op == MPI_MAX) {
-						for (int j = 0; j < count; ++j) {
-							if (static_cast<float *>(recvbuf)[j] < static_cast<float *>(sendbuf)[j])
-								static_cast<float *>(recvbuf)[j] = static_cast<float *>(sendbuf)[j];
-						}
-					} else if (op == MPI_MIN) {
-						for (int j = 0; j < count; ++j) {
-							if (static_cast<float *>(recvbuf)[j] > static_cast<float *>(sendbuf)[j])
-								static_cast<float *>(recvbuf)[j] = static_cast<float *>(sendbuf)[j];
-						}
-					} else if (op == MPI_SUM) {
-						for (int j = 0; j < count; ++j) {
-							static_cast<float *>(recvbuf)[j] += static_cast<float *>(sendbuf)[j];
-						}
-					} else if (op == MPI_PROD) {
-						for (int j = 0; j < count; ++j) {
-							static_cast<float *>(recvbuf)[j] *= static_cast<float *>(sendbuf)[j];
-						}
-					}
-				} else if (datatype == MPI_DOUBLE) {
-					if (op == MPI_MAX) {
-						for (int j = 0; j < count; ++j) {
-							if (static_cast<double *>(recvbuf)[j] < static_cast<double *>(sendbuf)[j])
-								static_cast<double *>(recvbuf)[j] = static_cast<double *>(sendbuf)[j];
-						}
-					} else if (op == MPI_MIN) {
-						for (int j = 0; j < count; ++j) {
-							if (static_cast<double *>(recvbuf)[j] > static_cast<double *>(sendbuf)[j])
-								static_cast<double *>(recvbuf)[j] = static_cast<double *>(sendbuf)[j];
-						}
-					} else if (op == MPI_SUM) {
-						for (int j = 0; j < count; ++j) {
-							static_cast<double *>(recvbuf)[j] += static_cast<double *>(sendbuf)[j];
-						}
-					} else if (op == MPI_PROD) {
-						for (int j = 0; j < count; ++j) {
-							static_cast<double *>(recvbuf)[j] *= static_cast<double *>(sendbuf)[j];
-						}
-					}
-				}
-				continue;
-			}
-			
-			MPI_Status status;
-			MPI_Recv(buffer, count, datatype, proc, 0, comm, &status);
-			if (datatype == MPI_INT) {
-				if (op == MPI_MAX) {
-					for (int j = 0; j < count; ++j) {
-						if (static_cast<int *>(recvbuf)[j] < static_cast<int *>(buffer)[j])
-							static_cast<int *>(recvbuf)[j] = static_cast<int *>(buffer)[j];
-					}
-				} else if (op == MPI_MIN) {
-					for (int j = 0; j < count; ++j) {
-						if (static_cast<int *>(recvbuf)[j] > static_cast<int *>(buffer)[j])
-							static_cast<int *>(recvbuf)[j] = static_cast<int *>(buffer)[j];
-					}
-				} else if (op == MPI_SUM) {
-					for (int j = 0; j < count; ++j) {
-						static_cast<int*>(recvbuf)[j] += static_cast<int*>(buffer)[j];
-					}
-				} else if (op == MPI_PROD) {
-					for (int j = 0; j < count; ++j) {
-						static_cast<int *>(recvbuf)[j] *= static_cast<int *>(buffer)[j];
-					}
-				}
-			} else if (datatype == MPI_FLOAT) {
-				if (op == MPI_MAX) {
-					for (int j = 0; j < count; ++j) {
-						if (static_cast<float *>(recvbuf)[j] < static_cast<float *>(buffer)[j])
-							static_cast<float *>(recvbuf)[j] = static_cast<float *>(buffer)[j];
-					}
-				} else if (op == MPI_MIN) {
-					for (int j = 0; j < count; ++j) {
-						if (static_cast<float *>(recvbuf)[j] > static_cast<float *>(buffer)[j])
-							static_cast<float *>(recvbuf)[j] = static_cast<float *>(buffer)[j];
-					}
-				} else if (op == MPI_SUM) {
-					for (int j = 0; j < count; ++j) {
-						static_cast<float *>(recvbuf)[j] += static_cast<float *>(buffer)[j];
-					}
-				} else if (op == MPI_PROD) {
-					for (int j = 0; j < count; ++j) {
-						static_cast<float *>(recvbuf)[j] *= static_cast<float *>(buffer)[j];
-					}
-				}
-			} else if (datatype == MPI_DOUBLE) {
-				if (op == MPI_MAX) {
-					for (int j = 0; j < count; ++j) {
-						if (static_cast<double *>(recvbuf)[j] < static_cast<double *>(buffer)[j])
-							static_cast<double *>(recvbuf)[j] = static_cast<double *>(buffer)[j];
-					}
-				} else if (op == MPI_MIN) {
-					for (int j = 0; j < count; ++j) {
-						if (static_cast<double *>(recvbuf)[j] > static_cast<double *>(buffer)[j])
-							static_cast<double *>(recvbuf)[j] = static_cast<double *>(buffer)[j];
-					}
-				} else if (op == MPI_SUM) {
-					for (int j = 0; j < count; ++j) {
-						static_cast<double *>(recvbuf)[j] += static_cast<double *>(buffer)[j];
-					}
-				} else if (op == MPI_PROD) {
-					for (int j = 0; j < count; ++j) {
-						static_cast<double *>(recvbuf)[j] *= static_cast<double *>(buffer)[j];
-					}
-				}
-			}
-		}
-	}
+
+    if (rank != root) {
+        MPI_Send(sendbuf, count, datatype, root, 0, comm);
+    }
+
+    if (rank == root) {
+        for (int proc = 0; proc < size; proc++) {
+            if (proc == root) {
+                if (datatype == MPI_INT) {
+                    if (op == MPI_MAX) {
+                        for (int j = 0; j < count; ++j) {
+                            if (static_cast<int *>(recvbuf)[j] < static_cast<int *>(sendbuf)[j])
+                                static_cast<int *>(recvbuf)[j] = static_cast<int *>(sendbuf)[j];
+                        }
+                    } else if (op == MPI_MIN) {
+                        for (int j = 0; j < count; ++j) {
+                            if (static_cast<int *>(recvbuf)[j] > static_cast<int *>(sendbuf)[j])
+                                static_cast<int *>(recvbuf)[j] = static_cast<int *>(sendbuf)[j];
+                        }
+                    } else if (op == MPI_SUM) {
+                        for (int j = 0; j < count; ++j) {
+                            static_cast<int*>(recvbuf)[j] += static_cast<int*>(sendbuf)[j];
+                        }
+                    } else if (op == MPI_PROD) {
+                        for (int j = 0; j < count; ++j) {
+                            static_cast<int *>(recvbuf)[j] *= static_cast<int *>(sendbuf)[j];
+                        }
+                    }
+                } else if (datatype == MPI_FLOAT) {
+                    if (op == MPI_MAX) {
+                        for (int j = 0; j < count; ++j) {
+                            if (static_cast<float *>(recvbuf)[j] < static_cast<float *>(sendbuf)[j])
+                                static_cast<float *>(recvbuf)[j] = static_cast<float *>(sendbuf)[j];
+                        }
+                    } else if (op == MPI_MIN) {
+                        for (int j = 0; j < count; ++j) {
+                            if (static_cast<float *>(recvbuf)[j] > static_cast<float *>(sendbuf)[j])
+                                static_cast<float *>(recvbuf)[j] = static_cast<float *>(sendbuf)[j];
+                        }
+                    } else if (op == MPI_SUM) {
+                        for (int j = 0; j < count; ++j) {
+                            static_cast<float *>(recvbuf)[j] += static_cast<float *>(sendbuf)[j];
+                        }
+                    } else if (op == MPI_PROD) {
+                        for (int j = 0; j < count; ++j) {
+                            static_cast<float *>(recvbuf)[j] *= static_cast<float *>(sendbuf)[j];
+                        }
+                    }
+                } else if (datatype == MPI_DOUBLE) {
+                    if (op == MPI_MAX) {
+                        for (int j = 0; j < count; ++j) {
+                            if (static_cast<double *>(recvbuf)[j] < static_cast<double *>(sendbuf)[j])
+                                static_cast<double *>(recvbuf)[j] = static_cast<double *>(sendbuf)[j];
+                        }
+                    } else if (op == MPI_MIN) {
+                        for (int j = 0; j < count; ++j) {
+                            if (static_cast<double *>(recvbuf)[j] > static_cast<double *>(sendbuf)[j])
+                                static_cast<double *>(recvbuf)[j] = static_cast<double *>(sendbuf)[j];
+                        }
+                    } else if (op == MPI_SUM) {
+                        for (int j = 0; j < count; ++j) {
+                            static_cast<double *>(recvbuf)[j] += static_cast<double *>(sendbuf)[j];
+                        }
+                    } else if (op == MPI_PROD) {
+                        for (int j = 0; j < count; ++j) {
+                            static_cast<double *>(recvbuf)[j] *= static_cast<double *>(sendbuf)[j];
+                        }
+                    }
+                }
+                continue;
+            }
+
+            MPI_Status status;
+            MPI_Recv(buffer, count, datatype, proc, 0, comm, &status);
+            if (datatype == MPI_INT) {
+                if (op == MPI_MAX) {
+                    for (int j = 0; j < count; ++j) {
+                        if (static_cast<int *>(recvbuf)[j] < static_cast<int *>(buffer)[j])
+                            static_cast<int *>(recvbuf)[j] = static_cast<int *>(buffer)[j];
+                    }
+                } else if (op == MPI_MIN) {
+                    for (int j = 0; j < count; ++j) {
+                        if (static_cast<int *>(recvbuf)[j] > static_cast<int *>(buffer)[j])
+                            static_cast<int *>(recvbuf)[j] = static_cast<int *>(buffer)[j];
+                    }
+                } else if (op == MPI_SUM) {
+                    for (int j = 0; j < count; ++j) {
+                        static_cast<int*>(recvbuf)[j] += static_cast<int*>(buffer)[j];
+                    }
+                } else if (op == MPI_PROD) {
+                    for (int j = 0; j < count; ++j) {
+                        static_cast<int *>(recvbuf)[j] *= static_cast<int *>(buffer)[j];
+                    }
+                }
+            } else if (datatype == MPI_FLOAT) {
+                if (op == MPI_MAX) {
+                    for (int j = 0; j < count; ++j) {
+                        if (static_cast<float *>(recvbuf)[j] < static_cast<float *>(buffer)[j])
+                            static_cast<float *>(recvbuf)[j] = static_cast<float *>(buffer)[j];
+                    }
+                } else if (op == MPI_MIN) {
+                    for (int j = 0; j < count; ++j) {
+                        if (static_cast<float *>(recvbuf)[j] > static_cast<float *>(buffer)[j])
+                            static_cast<float *>(recvbuf)[j] = static_cast<float *>(buffer)[j];
+                    }
+                } else if (op == MPI_SUM) {
+                    for (int j = 0; j < count; ++j) {
+                        static_cast<float *>(recvbuf)[j] += static_cast<float *>(buffer)[j];
+                    }
+                } else if (op == MPI_PROD) {
+                    for (int j = 0; j < count; ++j) {
+                        static_cast<float *>(recvbuf)[j] *= static_cast<float *>(buffer)[j];
+                    }
+                }
+            } else if (datatype == MPI_DOUBLE) {
+                if (op == MPI_MAX) {
+                    for (int j = 0; j < count; ++j) {
+                        if (static_cast<double *>(recvbuf)[j] < static_cast<double *>(buffer)[j])
+                            static_cast<double *>(recvbuf)[j] = static_cast<double *>(buffer)[j];
+                    }
+                } else if (op == MPI_MIN) {
+                    for (int j = 0; j < count; ++j) {
+                        if (static_cast<double *>(recvbuf)[j] > static_cast<double *>(buffer)[j])
+                            static_cast<double *>(recvbuf)[j] = static_cast<double *>(buffer)[j];
+                    }
+                } else if (op == MPI_SUM) {
+                    for (int j = 0; j < count; ++j) {
+                        static_cast<double *>(recvbuf)[j] += static_cast<double *>(buffer)[j];
+                    }
+                } else if (op == MPI_PROD) {
+                    for (int j = 0; j < count; ++j) {
+                        static_cast<double *>(recvbuf)[j] *= static_cast<double *>(buffer)[j];
+                    }
+                }
+            }
+        }
+    }
 
     if (datatype == MPI_INT)
         delete[] static_cast<int*>(buffer);
