c13bedd6e7caa7a26d040d7114aa935f3ebe8898
Maria Bakaeva <mary396969@gmail.com>
2020-11-20 20:18:53 +0300

    version4

--- modules/task_2/bakaeva_m_horizontal_scheme/gauss_horizontal_scheme.cpp
+++ modules/task_2/bakaeva_m_horizontal_scheme/gauss_horizontal_scheme.cpp
@@ -1,10 +1,10 @@
 // Copyright 2020 Bakaeva Maria
+#include "../../../modules/task_2/bakaeva_m_horizontal_scheme/gauss_horizontal_scheme.h"
 #include <mpi.h>
 #include <stddef.h>
 #include <math.h>
 #include <ctime>
 #include <random>
-#include "./gauss_horizontal_scheme.h"
 
 static int offset = 0;
 
@@ -13,19 +13,19 @@ void getSequentialGauss(const double* matrixGlobal, double* result, int size) {
     for (int i = 0; i < size * size + size; i++) {
         matrix[i] = matrixGlobal[i];
     }
-    
+
     // Прямой ход метода Гаусса
     int pivotRow;
     int* pivotPos = new int[size];
     int* pivotIter = new int[size];
-    
+
     for (int i = 0; i < size; i++) {
         pivotIter[i] = -1;
         pivotPos[i] = 0;
     }
-    
+
     int iter;  // Номер текущей итерации прямого хода
-    
+
     for (iter = 0; iter < size; iter++) {
         // Ищем ведущую строку
         double maxValue = 0;
@@ -35,10 +35,10 @@ void getSequentialGauss(const double* matrixGlobal, double* result, int size) {
                 maxValue = matrix[i * (size + 1) + iter];
                 }
         }
-        
+
         pivotPos[iter] = pivotRow;
         pivotIter[pivotRow] = iter;
-        
+
         // Вычитание
         double pivotValue, pivotFactor;
         pivotValue = matrix[pivotRow * (size + 1) + iter];
@@ -48,26 +48,26 @@ void getSequentialGauss(const double* matrixGlobal, double* result, int size) {
                 for (int j = iter; j < size; j++) {
                     matrix[i * (size + 1) + j] -= pivotFactor * matrix[pivotRow * (size + 1) + j];
                 }
-                
+
                 matrix[i * (size + 1) + size] -= pivotFactor * matrix[pivotRow * (size + 1) + size];
             }
         }
     }
-    
+
     // Обратный ход метода Гаусса
     int rowIdx, row;
     for (int i = (size - 1); i >= 0; i--) {
         rowIdx = pivotPos[i];
         result[i] = matrix[rowIdx * (size + 1) + size] / matrix[rowIdx * (size + 1) + i];
         matrix[rowIdx * (size + 1) + i] = 1;
-        
+
         for (int j = 0; j < i; j++) {
             row = pivotPos[j];
             matrix[row * (size + 1) + size] -= matrix[row * (size + 1) + i] * result[i];
             matrix[row * (size + 1) + i] = 0;
         }
     }
-    
+
     delete[] matrix;
     delete[] pivotPos;
     delete[] pivotIter;
@@ -78,29 +78,29 @@ void getParallelGauss(const double* matrixGlobal, double* result, int cols, int 
     for (int i = 0; i < rows * cols; i++) {
         matrix[i] = matrixGlobal[i];
     }
-    
+
     int procSize, rank;
     //Число задействованных процессов
     MPI_Comm_size(MPI_COMM_WORLD, &procSize);
     //Получение номера текущего процесса в рамках коммуникатора
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    
+
     // Количество строк для каждого процесса
     const int delta = rows / procSize;
     // Остаток
     const int remainder = rows % procSize;
-    
+
     int p;
     if (rank < remainder) {
         p = delta + 1;
     } else {
         p = delta;
     }
-    
+
     double* subMatrix = new double[cols * p];
     int* send_counts = new int[procSize];
     int* displs = new int[procSize];
-    
+
     displs[0] = 0;
     for (int i = 0; i < procSize; i++) {
         if (i < remainder) {
@@ -112,12 +112,12 @@ void getParallelGauss(const double* matrixGlobal, double* result, int cols, int 
             displs[i] = displs[i - 1] + send_counts[i - 1];
         }
     }
-    
+
     // Отправление данных всем процессам
     MPI_Scatterv(matrix, send_counts, displs, MPI_DOUBLE, subMatrix, p * cols, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    
+
     double* pivotRow = new double[cols];
-    
+
     for (int i = 0; i < displs[rank] / cols; i++) {
         int root = 0;
         int sum = 0;
@@ -129,9 +129,9 @@ void getParallelGauss(const double* matrixGlobal, double* result, int cols, int 
             }
             root++;
         }
-        
+
         MPI_Bcast(pivotRow, cols, MPI_DOUBLE, root, MPI_COMM_WORLD);
-        
+
         for (int j = 0; j < send_counts[rank] / cols; j++) {
             double s = pivotRow[i] / subMatrix[j * cols + i];
             for (int z = i; z < cols; z++) {
@@ -139,13 +139,13 @@ void getParallelGauss(const double* matrixGlobal, double* result, int cols, int 
             }
         }
     }
-    
+
     for (int i = 0; i < send_counts[rank] / cols; i++) {
         for (int j = 0; j < cols; j++) {
             pivotRow[j] = subMatrix[i * cols + j];
         }
         MPI_Bcast(pivotRow, cols, MPI_DOUBLE, rank, MPI_COMM_WORLD);
-        
+
         for (int j = i + 1; j < send_counts[rank] / cols; j++) {
             double s = pivotRow[displs[rank] / cols + i] / subMatrix[j * cols + i + displs[rank] / cols];
             for (int z = i + displs[rank] / cols; z < cols; z++) {
@@ -153,7 +153,7 @@ void getParallelGauss(const double* matrixGlobal, double* result, int cols, int 
             }
         }
     }
-    
+
     int root = 0;
     for (int i = displs[rank] / cols + send_counts[rank] / cols; i < rows; i++) {
         int sum = 0;
@@ -167,9 +167,9 @@ void getParallelGauss(const double* matrixGlobal, double* result, int cols, int 
         }
         MPI_Bcast(pivotRow, cols, MPI_DOUBLE, root, MPI_COMM_WORLD);
     }
-    
+
     MPI_Gatherv(subMatrix, p * cols, MPI_DOUBLE, matrix, send_counts, displs, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    
+
     if (rank == 0) {
         for (int i = rows - 1; i >= 0; i--) {
             double b = matrix[i * cols + cols - 1];
@@ -179,7 +179,7 @@ void getParallelGauss(const double* matrixGlobal, double* result, int cols, int 
             result[i] = b / matrix[i * cols + i];
         }
     }
-    
+
     delete[] matrix;
     delete[] subMatrix;
     delete[] send_counts;
--- modules/task_2/bakaeva_m_horizontal_scheme/main.cpp
+++ modules/task_2/bakaeva_m_horizontal_scheme/main.cpp
@@ -6,7 +6,7 @@
 TEST(GaussHorizontalScheme, testWorkWithMatrix3x4) {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    
+
     int rows = 3;
     int cols = 4;
     double* matrix = new double[rows * cols];
@@ -14,7 +14,7 @@ TEST(GaussHorizontalScheme, testWorkWithMatrix3x4) {
     matrix[0] = 8; matrix[1] = 7; matrix[2] = 3; matrix[3] = 18;
     matrix[4] = -7; matrix[5] = -4; matrix[6] = -4; matrix[7] = -11;
     matrix[8] = -6; matrix[9] = 5; matrix[10] = -4; matrix[11] = -15;
-    
+
     getParallelGauss(matrix, resParallel, cols, rows);
 
     if (rank == 0) {
@@ -23,10 +23,10 @@ TEST(GaussHorizontalScheme, testWorkWithMatrix3x4) {
         for (int i = 0; i < rows; i++) {
             ASSERT_NEAR(resLinear[i], resParallel[i], 0.00001);
         }
-        
+
         delete[] resLinear;
     }
-    
+
     delete[] matrix;
     delete[] resParallel;
 }
@@ -34,7 +34,7 @@ TEST(GaussHorizontalScheme, testWorkWithMatrix3x4) {
 TEST(GaussHorizontalScheme, testWorkWithMatrix4x5) {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    
+
     int rows = 4;
     int cols = 5;
     double* matrix = new double[rows * cols];
@@ -43,7 +43,7 @@ TEST(GaussHorizontalScheme, testWorkWithMatrix4x5) {
     matrix[5] = 1; matrix[6] = -2; matrix[7] = 3; matrix[8] = -1; matrix[9] = 14;
     matrix[10] = 3; matrix[11] = -1; matrix[12] = -1; matrix[13] = -2; matrix[14] = 7;
     matrix[15] = 2; matrix[16] = 3; matrix[17] = -1; matrix[18] = -1; matrix[19] = 9;
-    
+
     getParallelGauss(matrix, resParallel, cols, rows);
 
     if (rank == 0) {
@@ -54,7 +54,7 @@ TEST(GaussHorizontalScheme, testWorkWithMatrix4x5) {
         }
         delete[] resLinear;
     }
-    
+
     delete[] matrix;
     delete[] resParallel;
 }
@@ -62,14 +62,14 @@ TEST(GaussHorizontalScheme, testWorkWithMatrix4x5) {
 TEST(GaussHorizontalScheme, testWorkWithMatrix2x3) {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    
+
     int rows = 2;
     int cols = 3;
     double* matrix = new double[rows * cols];
     double* resParallel = new double[rows];
     matrix[0] = 1; matrix[1] = 2; matrix[2] = 5;
     matrix[3] = 3; matrix[4] = 3000; matrix[5] = 7;
-    
+
     getParallelGauss(matrix, resParallel, cols, rows);
 
     if (rank == 0) {
@@ -80,7 +80,7 @@ TEST(GaussHorizontalScheme, testWorkWithMatrix2x3) {
         }
         delete[] resLinear;
     }
-    
+
     delete[] matrix;
     delete[] resParallel;
 }
@@ -88,7 +88,7 @@ TEST(GaussHorizontalScheme, testWorkWithMatrix2x3) {
 TEST(GaussHorizontalScheme, testWorkWithMatrix5x6) {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    
+
     int rows = 5;
     int cols = 6;
     double* matrix = new double[rows * cols];
@@ -98,7 +98,7 @@ TEST(GaussHorizontalScheme, testWorkWithMatrix5x6) {
     matrix[12] = -36; matrix[13] = 23; matrix[14] = 5; matrix[15] = 1; matrix[16] = 7; matrix[17] = 71;
     matrix[18] = 5; matrix[19] = -8; matrix[20] = 1; matrix[21] = 6; matrix[22] = 6; matrix[23] = 4;
     matrix[24] = 25; matrix[25] = 8; matrix[26] = 4; matrix[27] = 6; matrix[28] = 5; matrix[29] = 0;
-    
+
     getParallelGauss(matrix, resParallel, cols, rows);
 
     if (rank == 0) {
@@ -109,7 +109,7 @@ TEST(GaussHorizontalScheme, testWorkWithMatrix5x6) {
         }
         delete[] resLinear;
     }
-    
+
     delete[] matrix;
     delete[] resParallel;
 }
@@ -117,7 +117,7 @@ TEST(GaussHorizontalScheme, testWorkWithMatrix5x6) {
 TEST(GaussHorizontalScheme, testWorkWithMatrix6x7) {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    
+
     int rows = 6;
     int cols = 7;
     double* matrix = new double[rows * cols];
@@ -129,7 +129,7 @@ TEST(GaussHorizontalScheme, testWorkWithMatrix6x7) {
     matrix[24] = 15; matrix[25] = 0; matrix[26] = 25; matrix[27] = 45; matrix[28] = -6; matrix[29] = 5;
     matrix[30] = -4; matrix[31] = 4; matrix[32] = 8; matrix[33] = 9; matrix[34] = 1; matrix[35] = 44;
     matrix[36] = 4; matrix[37] = 3; matrix[38] = 2; matrix[39] = 1; matrix[40] = 5; matrix[41] = 6;
-    
+
     getParallelGauss(matrix, resParallel, cols, rows);
 
     if (rank == 0) {
@@ -140,12 +140,12 @@ TEST(GaussHorizontalScheme, testWorkWithMatrix6x7) {
         }
         delete[] resLinear;
     }
-    
+
     delete[] matrix;
     delete[] resParallel;
 }
 
-int main(int argc, char** argv) {    
+int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     MPI_Init(&argc, &argv);
 
