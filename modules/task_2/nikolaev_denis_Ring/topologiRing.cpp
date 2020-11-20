4b06a50ab7c4c372834933e95665608870b74cbf
Denis <den_nikolaev_2000@mail.ru>
2020-11-21 02:50:35 +0300

    first

---
+++ modules/task_2/nikolaev_denis_Ring/topologiRing.cpp
@@ -0,0 +1,63 @@
+// Copyright 2020 Nikolaev Denis
+#include "../../../modules/task_2/nikolaev_denis_Ring/topologiRing.h"
+#include <mpi.h>
+
+int* randomizeArray(int size, int l, int r) {
+    srand(time(NULL));
+    std::mt19937 gen(time(0));
+    std::uniform_real_distribution<> urx(l, r);
+    int* array = new int[size];
+    for (int i = 0; i < size; i++) {
+        array[i] = urx(gen);
+    }
+    return array;
+}
+MPI_Comm topologyRing(MPI_Comm oldComm) {
+    int* dims = new int[NDims];
+    int* periods = new int[NDims];
+    periods[0] = 1;
+    MPI_Comm_size(oldComm, &dims[0]);
+    MPI_Comm ringTop;
+    MPI_Cart_create(oldComm, NDims, dims, periods, Reorder, &ringTop);
+    return ringTop;
+}
+int* Send(int* local_arr, int arraySize, int currentRank, int finiteRank, MPI_Comm ringTop) {
+    int ProcNum, ProcRank;
+    MPI_Comm_size(ringTop, &ProcNum);
+    MPI_Comm_rank(ringTop, &ProcRank);
+    int* res = new int[arraySize];
+    if (currentRank == finiteRank) {
+        return local_arr;
+    }
+    int local_current_rank, local_dest_rank;
+    MPI_Status status;
+    MPI_Request request;
+    MPI_Cart_shift(ringTop, 0, 1, &local_current_rank, &local_dest_rank);
+    if (ProcRank == currentRank) {
+        MPI_Isend(&local_arr[0], arraySize, MPI_INT, local_dest_rank, 0, ringTop, &request);
+        MPI_Wait(&request, &status);
+        free(local_arr);
+        free(res);
+    } else if (currentRank > finiteRank &&
+        ((ProcRank > currentRank && ProcRank < ProcNum) || (ProcRank < finiteRank))) {
+        MPI_Irecv(&res[0], arraySize, MPI_INT, local_current_rank, 0, ringTop, &request);
+        MPI_Wait(&request, &status);
+        MPI_Isend(&local_arr[0], arraySize, MPI_INT, local_dest_rank, 0, ringTop, &request);
+        MPI_Wait(&request, &status);
+        free(local_arr);
+        free(res);
+    } else if (currentRank < finiteRank && ProcRank > currentRank && ProcRank < finiteRank) {
+        MPI_Irecv(&res[0], arraySize, MPI_INT, local_current_rank, 0, ringTop, &request);
+        MPI_Wait(&request, &status);
+        MPI_Isend(&local_arr[0], arraySize, MPI_INT, local_dest_rank, 0, ringTop, &request);
+        MPI_Wait(&request, &status);
+        free(local_arr);
+        free(res);
+    } else if (ProcRank == finiteRank) {
+        MPI_Irecv(&res[0], arraySize, MPI_INT, local_current_rank, 0, ringTop, &request);
+        MPI_Wait(&request, &status);
+        free(local_arr);
+        free(res);
+    }
+    return res;
+}
