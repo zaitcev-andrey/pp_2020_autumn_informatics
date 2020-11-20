7b46cd51eaf7962debbde8f91e3694309f0df227
MacBookUser <macbookuser@MacBook-Air-MacBookUser.local>
2020-11-20 23:53:12 +0300

    Fouth

--- modules/task_2/tkachev_a_ribbon_horizontal_scheme/main.cpp
+++ modules/task_2/tkachev_a_ribbon_horizontal_scheme/main.cpp
@@ -40,9 +40,76 @@ TEST(Ribbon_horizontal_scheme, MatrixTest_101_503) {
     }
 }
 
-TEST(Ribbon_horizontal_scheme, MatrixTest_100_1) {
+TEST(Ribbon_horizontal_scheme, MatrixTest_42_59) {
     int process_rank;
-    int matrix_count_rows = 100, matrix_count_columns = 1;
+    int matrix_count_rows = 42, matrix_count_columns = 59;
+    std::vector<int> test_matrix(matrix_count_rows * matrix_count_columns);
+
+    std::vector<int> the_test_vector(matrix_count_columns);
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &process_rank);
+
+    if (process_rank == 0) {
+       test_matrix = getRandomVector(matrix_count_rows, matrix_count_columns);
+       the_test_vector = getRandomVector(matrix_count_columns, 1);
+    }
+
+    std::vector<int> parallel_multiply_on_vector;
+
+    double _time1 = MPI_Wtime();
+    parallel_multiply_on_vector = parallelMultiplyOnVector(
+                test_matrix, the_test_vector, matrix_count_rows, matrix_count_columns);
+    double _time2 = MPI_Wtime();
+
+    if (process_rank == 0) {
+        printf("PARRALEL TIME: %.5f\n", _time2-_time1);
+        std::vector<int> multiply_on_vector;
+        _time1 = MPI_Wtime();
+        multiply_on_vector = multiplyOnVector(test_matrix, the_test_vector,
+        matrix_count_rows, matrix_count_columns);
+        _time2 = MPI_Wtime();
+        printf("NOT PARRALEL TIME %.5f\n", _time2-_time1);
+        ASSERT_EQ(multiply_on_vector, parallel_multiply_on_vector);
+    }
+}
+
+TEST(Ribbon_horizontal_scheme, MatrixTest_90_90) {
+    int process_rank;
+    int matrix_count_rows = 90, matrix_count_columns = 90;
+    std::vector<int> test_matrix(matrix_count_rows * matrix_count_columns);
+
+    std::vector<int> the_test_vector(matrix_count_columns);
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &process_rank);
+
+    if (process_rank == 0) {
+       test_matrix = getRandomVector(matrix_count_rows, matrix_count_columns);
+       the_test_vector = getRandomVector(matrix_count_columns, 1);
+    }
+
+    std::vector<int> parallel_multiply_on_vector;
+
+    double _time1 = MPI_Wtime();
+    parallel_multiply_on_vector = parallelMultiplyOnVector(
+                test_matrix, the_test_vector, matrix_count_rows, matrix_count_columns);
+    double _time2 = MPI_Wtime();
+
+    if (process_rank == 0) {
+        printf("PARRALEL TIME: %.5f\n", _time2-_time1);
+        std::vector<int> multiply_on_vector;
+        _time1 = MPI_Wtime();
+        multiply_on_vector = multiplyOnVector(test_matrix, the_test_vector,
+        matrix_count_rows, matrix_count_columns);
+        _time2 = MPI_Wtime();
+        printf("NOT PARRALEL TIME %.5f\n", _time2-_time1);
+        ASSERT_EQ(multiply_on_vector, parallel_multiply_on_vector);
+    }
+}
+
+
+TEST(Ribbon_horizontal_scheme, MatrixTest_100_10) {
+    int process_rank;
+    int matrix_count_rows = 100, matrix_count_columns = 10;
     std::vector<int> test_matrix(matrix_count_rows * matrix_count_columns);
 
     std::vector<int> the_test_vector(matrix_count_columns);
