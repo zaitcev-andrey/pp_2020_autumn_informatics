aa1724e45f420d9a63a4005c73d9ae6a0a94a004
Denis Kulandin <kulandin08@gmail.com>
2020-11-20 15:05:18 +0300

    space

--- modules/task_1/kulandin_d_matrix_sum/matrix_sum.cpp
+++ modules/task_1/kulandin_d_matrix_sum/matrix_sum.cpp
@@ -28,6 +28,7 @@ int64_t getParallelSum(const std::vector<int> &a, int n) {
             MPI_Send(a.data() + rem + j * delta, delta, MPI_INT, j, 0, MPI_COMM_WORLD);
         }
     }
+    
     std::vector<int> local(rank == 0 ? rem + delta : delta);
     if (rank == 0) {
         local = {a.begin(), a.begin() + delta + rem};
