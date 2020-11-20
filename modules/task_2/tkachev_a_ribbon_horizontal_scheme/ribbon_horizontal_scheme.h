2c47ae583623909e2bd4620596ff017eb87743c7
MacBookUser <macbookuser@MacBook-Air-MacBookUser.local>
2020-11-20 23:12:59 +0300

    Second

--- modules/task_2/tkachev_a_ribbon_horizontal_scheme/ribbon_horizontal_scheme.h
+++ modules/task_2/tkachev_a_ribbon_horizontal_scheme/ribbon_horizontal_scheme.h
@@ -1,6 +1,6 @@
 // Copyright 2020 Tkachev Alexey
-#ifndef MODULES_TASK_2_TKACHEV_A_RIBBON_HORIZONTAL_SCHEME_RIBBON_HORIZONTAL_SCHEME_H
-#define MODULES_TASK_2_TKACHEV_A_RIBBON_HORIZONTAL_SCHEME_RIBBON_HORIZONTAL_SCHEME_H
+#ifndef MODULES_TASK_2_TKACHEV_A_RIBBON_HORIZONTAL_SCHEME_RIBBON_HORIZONTAL_SCHEME_H_
+#define MODULES_TASK_2_TKACHEV_A_RIBBON_HORIZONTAL_SCHEME_RIBBON_HORIZONTAL_SCHEME_H_
 #include <vector>
 
 std::vector<int> getRandomVector(int count_rows = 0, int count_columns = 0);
@@ -11,4 +11,4 @@ std::vector<int> multiplyOnVector(std::vector<int> matrix, std::vector<int> the_
 std::vector<int> parallelMultiplyOnVector(std::vector<int> matrix, std::vector<int> the_vector,
                                           int matrix_count_rows = 0, int matrix_count_columns = 0);
 
-#endif  // MODULES_TASK_2_TKACHEV_A_BAND_HORIZONTAL_SCHEME_RIBBON_HORIZONTAL_SCHEME_H
+#endif  // MODULES_TASK_2_TKACHEV_A_RIBBON_HORIZONTAL_SCHEME_RIBBON_HORIZONTAL_SCHEME_H_
