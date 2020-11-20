b08d0d6f7157d29e342ebc74bb4687beac0a1b3f
ParanichevaAlyona <43447194+ParanichevaAlyona@users.noreply.github.com>
2020-11-20 18:17:25 +0300

    Update gather.h

--- modules/task_2/paranicheva_a_gather/gather.h
+++ modules/task_2/paranicheva_a_gather/gather.h
@@ -1,6 +1,6 @@
 // Copyright 2020 Paranicheva Alyona
-#ifndef MODULES_TASK_1_PARANICHEVA_A_GATHER_GATHER_H_
-#define MODULES_TASK_1_PARANICHEVA_A_GATHER_GATHER_H_
+#ifndef MODULES_TASK_2_PARANICHEVA_A_GATHER_GATHER_H_
+#define MODULES_TASK_2_PARANICHEVA_A_GATHER_GATHER_H_
 
 #include <vector>
 
@@ -9,4 +9,4 @@ int Gather(void* sbuf, int scount, MPI_Datatype stype,
 std::vector<int> getRandomArrInt(int number, int rank);
 std::vector<double> getRandomArrDouble(int number, int rank);
 
-#endif  // MODULES_TASK_1_PARANICHEVA_A_GATHER_GATHER_H_
+#endif  // MODULES_TASK_2_PARANICHEVA_A_GATHER_GATHER_H_
