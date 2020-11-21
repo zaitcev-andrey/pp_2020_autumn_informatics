e3442de6f5ddff14f1c9dfb6a221594c965b5ca4
Nesterov Alexander <nesoldr@gmail.com>
2020-11-21 03:22:56 +0300

    Revert "Прокофьева Елизавета. Задача 2. Линейка."

--- modules/task_2/prokofeva_e_linear_array/linear_array.h
+++
@@ -1,9 +0,0 @@
-// Copyright 2020 Prokofeva Elizaveta
-#ifndef MODULES_TASK_2_PROKOFEVA_E_LINEAR_ARRAY_LINEAR_ARRAY_H_
-#define MODULES_TASK_2_PROKOFEVA_E_LINEAR_ARRAY_LINEAR_ARRAY_H_
-
-int* getRandomVector(int size);
-MPI_Comm create_comm(MPI_Comm comm);
-int* send_recv(int* buf, int count, MPI_Datatype type, int dest, int tag, MPI_Comm comm, int source, int ProcRank);
-
-#endif  // MODULES_TASK_2_PROKOFEVA_E_LINEAR_ARRAY_LINEAR_ARRAY_H_
