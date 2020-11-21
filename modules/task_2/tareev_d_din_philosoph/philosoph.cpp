fd9e1a1eb191cacf30341fbf02f683c39c5e72bb
Daniil <d_tareev@mail.ru>
2020-11-21 20:44:26 +0300

    five

--- modules/task_2/tareev_d_din_philosoph/philosoph.cpp
+++ modules/task_2/tareev_d_din_philosoph/philosoph.cpp
@@ -39,8 +39,7 @@ void table(bool* dinner, bool* fork, int ProcSize, std::list<int> ph_queue) {
                 fork[philosopher % (ProcSize - 1)] = taken;
                 fork[philosopher - 1] = taken;
                 MPI_Send(&out_buffer, 1, MPI_INT, philosopher, FORK_GET, MPI_COMM_WORLD);
-            }
-            else {
+            } else {
                 ph_queue.push_back(philosopher);
             }
         }
@@ -105,8 +104,7 @@ void diningPhilosofers(int myRank, int ProcSize, int time) {
         bool* fork = new bool[ProcSize - 1];
         if (myRank == 0) {
             table(dinner, fork, ProcSize, ph_queue);
-        }
-        else {
+        } else {
             philosofers(time);
         }
     }
