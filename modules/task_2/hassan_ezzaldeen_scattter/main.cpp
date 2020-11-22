5b596c63380253e5ada66baef5a7f8a7f57eb0be
EzzAldeen <49552293+EzzAldeenHassan@users.noreply.github.com>
2020-11-23 01:52:34 +0300

    Update main.cpp

--- modules/task_2/hassan_ezzaldeen_scattter/main.cpp
+++ modules/task_2/hassan_ezzaldeen_scattter/main.cpp
@@ -3,7 +3,6 @@
 #include <gtest-mpi-listener.hpp>
 #include <string>
 #include <vector>
-#include "./hassan_ezzaldeen_scattter.h"
 #include "../../../modules/task_2/hassan_ezzaldeen_scattter/scatter.h"
 
 TEST(scatter, throw_when_different_size) {
