2a20554118c3417eb953e9055d69cad88ed60cc5
EzzAldeen <49552293+EzzAldeenHassan@users.noreply.github.com>
2020-11-23 01:33:01 +0300

    Update scatter.h

--- modules/task_2/hassan_ezzaldeen_scattter/scatter.h
+++ modules/task_2/hassan_ezzaldeen_scattter/scatter.h
@@ -6,9 +6,8 @@
 #include <cstring>
 #include <vector>
 
-int MPI_Scatter_custom(void* sendBuf, int sendCount,
-    MPI_Datatype sendType, void* recvBuf,
-    int recvCount, MPI_Datatype recvType,
-    int ROOT, MPI_Comm COMM);
+int MPI_Scatter_custom(void* sendbuf, int sendcount, MPI_Datatype sendtype,
+                       void* recvbuf, int recvcount, MPI_Datatype recvtype,
+                       int root, MPI_Comm comm);
 
 #endif  // MODULES_TASK_2_HASSAN_EZZALDEEN_SCATTTER_H_
