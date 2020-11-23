fd7513c913840b67707c38d94c770377a96ded8a
EzzAldeen <49552293+EzzAldeenHassan@users.noreply.github.com>
2020-11-23 11:37:07 +0300

    Fixed

--- modules/task_2/hassan_ezzaldeen_scattter/scatter.h
+++ modules/task_2/hassan_ezzaldeen_scattter/scatter.h
@@ -1,6 +1,6 @@
 // Copyright 2020 Hassan EzzAldeen
-#ifndef MODULES_TASK_2_HASSAN_EZZALDEEN_SCATTTER_H_
-#define MODULES_TASK_2_HASSAN_EZZALDEEN_SCATTTER_H_
+#ifndef MODULES_TASK_2_HASSAN_EZZALDEEN_SCATTTER_SCATTER_H_
+#define MODULES_TASK_2_HASSAN_EZZALDEEN_SCATTTER_SCATTER_H_
 #include <mpi.h>
 #include <string>
 #include <cstring>
@@ -10,4 +10,5 @@ int MPI_Scatter_custom(void* sendbuf, int sendcount, MPI_Datatype sendtype,
                        void* recvbuf, int recvcount, MPI_Datatype recvtype,
                        int root, MPI_Comm comm);
 
-#endif  // MODULES_TASK_2_HASSAN_EZZALDEEN_SCATTTER_H_
+#endif  // MODULES_TASK_2_HASSAN_EZZALDEEN_SCATTTER_SCATTER_H_
+
