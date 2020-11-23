82f3865a60097c7ae9a9f9f134632c00c8be5439
KasjanychevMikhail <kasmihan@rambler.ru>
2020-11-23 22:42:29 +0300

    task2ver2

--- modules/task_2/kasyanychev_m_producer_consumers/producer_consumers.cpp
+++ modules/task_2/kasyanychev_m_producer_consumers/producer_consumers.cpp
@@ -2,8 +2,7 @@
 #include <mpi.h>
 #include <time.h>
 #include <random>
-#include <iostream>
-#include "./producer_consumers.h"
+#include "../../../modules/task_2/kasyanychev_m_producer_consumers/producer_consumers.h"
 
 int* generateNum(int size) {
     int* res = new int[size];
@@ -34,7 +33,7 @@ void producerConsumers(int* nums, double* res, int sizeArr) {
             MPI_Status status;
             double tmp = 0;
             MPI_Recv(&tmp, 1, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-            
+
             if (status.MPI_TAG > 0) {
                 res[status.MPI_TAG - 1] = tmp;
             }
@@ -53,8 +52,7 @@ void producerConsumers(int* nums, double* res, int sizeArr) {
 
             MPI_Send(&i, 1, MPI_INT, status.MPI_SOURCE, 0, MPI_COMM_WORLD);
         }
-    }
-    else {
+    } else {
         double tmp = 0;
         MPI_Send(&tmp, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
         bool flag = true;
@@ -63,9 +61,9 @@ void producerConsumers(int* nums, double* res, int sizeArr) {
             int num = 0;
             MPI_Status status;
             MPI_Recv(&num, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-            if (status.MPI_TAG == 0)
+            if (status.MPI_TAG == 0) {
                 flag = false;
-            else {
+            } else {
                 tmp = sqrt(num);
                 MPI_Send(&tmp, 1, MPI_DOUBLE, 0, status.MPI_TAG, MPI_COMM_WORLD);
             }
