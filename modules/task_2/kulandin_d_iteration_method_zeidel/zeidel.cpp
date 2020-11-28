d71b159dc384c3d2de3fc2a32263634c360dbb38
Nesterov Alexander <nesoldr@gmail.com>
2020-11-28 19:21:16 +0300

    Revert "Куландин Денис. Задача 2. Итеративные методы Зейделя. Исправления"

--- modules/task_2/kulandin_d_iteration_method_zeidel/zeidel.cpp
+++ modules/task_2/kulandin_d_iteration_method_zeidel/zeidel.cpp
@@ -29,20 +29,16 @@ std::vector<double> randomVectorB(const size_t sz) {
     gen.seed(static_cast<unsigned int>(time(0)));
     std::vector<double> ans(sz);
     for (size_t i = 0; i < sz; ++i) {
-        ans[i] = gen() % 1000;
+        ans[i] = gen() % 100;
     }
     return ans;
 }
 
-bool converge(const std::vector<double>& a, const std::vector<double>& x,
-              const std::vector<double>& b, size_t n, double eps) {
+bool converge(const std::vector<double>& x, const std::vector<double>& last, double eps) {
     double ans = 0;
-    for (size_t i = 0; i < n; ++i) {
-        double val = 0;
-        for (size_t j = 0; j < n; ++j) {
-            val += a[i * n + j] * x[j];
-        }
-        ans += std::pow(val - b[i], 2);
+    for (size_t i = 0; i < x.size(); ++i) {
+        double tmp = x[i] - last[i];
+        ans += tmp * tmp;
     }
     return sqrt(ans) < eps;
 }
@@ -50,8 +46,9 @@ bool converge(const std::vector<double>& a, const std::vector<double>& x,
 std::pair<bool, std::vector<double> > zeidelSequential(const std::vector<double>& a, const std::vector<double>& b,
                                                        size_t n, double eps) {
     int cntIterations = 100;
-    std::vector<double> x(n, 0);
+    std::vector<double> x(n, 0), last(n, 0);
     do {
+        last = x;
         for (size_t i = 0; i < n; ++i) {
             x[i] = 0;
             double gg = 0;
@@ -60,7 +57,7 @@ std::pair<bool, std::vector<double> > zeidelSequential(const std::vector<double>
             }
             x[i] = (b[i] - gg) / a[i * n + i];
         }
-    } while (!converge(a, x, b, n, eps) && cntIterations--);
+    } while (!converge(x, last, eps) && cntIterations--);
     return std::make_pair(cntIterations != 0, x);
 }
 
@@ -101,15 +98,16 @@ std::pair<bool, std::vector<double> > zeidelParallel(const std::vector<double>& 
     MPI_Comm_size(MPI_COMM_WORLD, &procNum);
     MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
     int cntIterations = 100;
-    std::vector<double> x(n, 0);
+    std::vector<double> x(n, 0), last(n, 0);
     char result;
     do {
+        last = x;
         for (size_t i = 0; i < n; ++i) {
             x[i] = 0;
             double gg = calcParallel(a, x, i, n);
             x[i] = (b[i] - gg) / a[i * n + i];
         }
-        if (!converge(a, x, b, n, eps) && cntIterations--) {
+        if (!converge(x, last, eps) && cntIterations--) {
             result = 1;
         } else {
             result = 0;
