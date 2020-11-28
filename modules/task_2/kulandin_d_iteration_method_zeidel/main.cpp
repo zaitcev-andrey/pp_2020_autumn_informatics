26cd003bc1fecc3fc8bcfe5b8cb267b18911253a
Nesterov Alexander <nesoldr@gmail.com>
2020-11-28 19:22:49 +0300

    Revert "Revert "Куландин Денис. Задача 2. Итеративные методы Зейделя. Исправления""

--- modules/task_2/kulandin_d_iteration_method_zeidel/main.cpp
+++ modules/task_2/kulandin_d_iteration_method_zeidel/main.cpp
@@ -6,27 +6,27 @@
 #include <vector>
 #include "./zeidel.h"
 
-const double EPS = 1e-8;
-
 TEST(Parallel_MPI, Test_1) {
     int procNum, procRank;
     MPI_Comm_size(MPI_COMM_WORLD, &procNum);
     MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
     size_t n = 8;
-    double eps = 1e-9;
+    double eps = 1e-4;
     std::vector<double> a(n * n, 1);
     std::vector<double> b(n, 1);
     if (!procRank) {
-        a[0] = 61; a[1] = 2;  a[2] = 3;   a[3] = 4;   a[4] = 6;   a[5] = 8;   a[6] = 9;   a[7] = 2;
-        a[8] = 2;  a[9] = 84; a[10] = 6;  a[11] = 4;  a[12] = 3;  a[13] = 2;  a[14] = 8;  a[15] = 7;
-        a[16] = 3; a[17] = 6; a[18] = 68; a[19] = 2;  a[20] = 4;  a[21] = 3;  a[22] = 9;  a[23] = 1;
-        a[24] = 4; a[25] = 4; a[26] = 2;  a[27] = 59; a[28] = 6;  a[29] = 4;  a[30] = 3;  a[31] = 8;
-        a[32] = 6; a[33] = 3; a[34] = 4;  a[35] = 6;  a[36] = 93; a[36] = 8;  a[38] = 3;  a[39] = 1;
-        a[40] = 8; a[41] = 2; a[42] = 3;  a[43] = 4;  a[44] = 8;  a[45] = 98; a[46] = 3;  a[47] = 1;
-        a[48] = 9; a[49] = 8; a[50] = 9;  a[51] = 3;  a[52] = 3;  a[53] = 3;  a[54] = 85; a[55] = 7;
-        a[56] = 2; a[57] = 7; a[58] = 1;  a[59] = 8;  a[60] = 1;  a[61] = 1;  a[62] = 7;  a[63] = 98;
+        a = {
+            61, 2,  3,  4,  6,  8,  9,  2,
+            2, 84,  6,  4,  3,  2,  8,  7,
+            3,  6, 68,  2,  4,  3,  9,  1,
+            4,  4,  2, 59,  6,  4,  3,  8,
+            6,  3,  4,  6, 93,  8,  3,  1,
+            8,  2,  3,  4,  8, 98,  3,  1,
+            9,  8,  9,  3,  3,  3, 85,  7,
+            2,  7,  1,  8,  1,  1,  7, 98
+        };
 
-        b[0] = 95; b[1] = 116; b[2] = 96; b[3] = 90; b[4] = 124; b[5] = 127; b[6] = 127; b[7] = 125;
+        b = {95, 116, 96, 90, 124, 127, 127, 125};
     }
 
     double timeParallel = MPI_Wtime();
@@ -39,21 +39,15 @@ TEST(Parallel_MPI, Test_1) {
         timeSequential = MPI_Wtime() - timeSequential;
         ASSERT_EQ(ans.first, seq.first);
         if (!ans.first) return;
-        double mse = 0;
-        for (size_t i = 0; i < n; ++i) {
-            mse += (ans.second[i] - seq.second[i]) * (ans.second[i] - seq.second[i]);
-        }
-        mse = sqrt(mse);
-        ASSERT_LE(mse, EPS);
         double err = 0;
         for (size_t i = 0; i < n; ++i) {
             double su = 0;
             for (size_t j = 0; j < n; ++j) {
                 su += a[i * n + j] * ans.second[j];
             }
-            ASSERT_LE(fabs(su - b[i]), sqrt(eps));
-            err += fabs(su - b[i]);
+            err += std::pow(fabs(su - b[i]), 2);
         }
+        ASSERT_LE(sqrt(err), eps);
         printf("Summary error:              %.10f\n", err);
         printf("Parallel time:              %.10f\n", timeParallel);
         printf("Sequential time:            %.10f\n", timeSequential);
@@ -65,7 +59,7 @@ TEST(Parallel_MPI, Test_2) {
     MPI_Comm_size(MPI_COMM_WORLD, &procNum);
     MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
     size_t n = 3;
-    double eps = 1e-9;
+    double eps = 1e-4;
     std::vector<double> a(n * n, 1);
     std::vector<double> b(n, 1);
     if (!procRank) {
@@ -87,21 +81,15 @@ TEST(Parallel_MPI, Test_2) {
         timeSequential = MPI_Wtime() - timeSequential;
         ASSERT_EQ(ans.first, seq.first);
         if (!ans.first) return;
-        double mse = 0;
-        for (size_t i = 0; i < n; ++i) {
-            mse += (ans.second[i] - seq.second[i]) * (ans.second[i] - seq.second[i]);
-        }
-        mse = sqrt(mse);
-        ASSERT_LE(mse, EPS);
         double err = 0;
         for (size_t i = 0; i < n; ++i) {
             double su = 0;
             for (size_t j = 0; j < n; ++j) {
                 su += a[i * n + j] * ans.second[j];
             }
-            ASSERT_LE(fabs(su - b[i]), sqrt(eps));
-            err += fabs(su - b[i]);
+            err += std::pow(fabs(su - b[i]), 2);
         }
+        ASSERT_LE(sqrt(err), eps);
         printf("Summary error:              %.10f\n", err);
         printf("Parallel time:              %.10f\n", timeParallel);
         printf("Sequential time:            %.10f\n", timeSequential);
@@ -114,7 +102,7 @@ TEST(Parallel_MPI, Test_3) {
     MPI_Comm_size(MPI_COMM_WORLD, &procNum);
     MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
     size_t n = 3;
-    double eps = 1e-9;
+    double eps = 1e-4;
     std::vector<double> a(n * n, 1);
     std::vector<double> b(n, 1);
     if (!procRank) {
@@ -136,21 +124,15 @@ TEST(Parallel_MPI, Test_3) {
         timeSequential = MPI_Wtime() - timeSequential;
         ASSERT_EQ(ans.first, seq.first);
         if (!ans.first) return;
-        double mse = 0;
-        for (size_t i = 0; i < n; ++i) {
-            mse += (ans.second[i] - seq.second[i]) * (ans.second[i] - seq.second[i]);
-        }
-        mse = sqrt(mse);
-        ASSERT_LE(mse, EPS);
         double err = 0;
         for (size_t i = 0; i < n; ++i) {
             double su = 0;
             for (size_t j = 0; j < n; ++j) {
                 su += a[i * n + j] * ans.second[j];
             }
-            ASSERT_LE(fabs(su - b[i]), sqrt(eps));
-            err += fabs(su - b[i]);
+            err += std::pow(fabs(su - b[i]), 2);
         }
+        ASSERT_LE(sqrt(err), eps);
         printf("Summary error:              %.10f\n", err);
         printf("Parallel time:              %.10f\n", timeParallel);
         printf("Sequential time:            %.10f\n", timeSequential);
@@ -162,7 +144,7 @@ TEST(Parallel_MPI, Test_4) {
     MPI_Comm_size(MPI_COMM_WORLD, &procNum);
     MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
     size_t n = 100;
-    double eps = 1e-9;
+    double eps = 1e-4;
     std::vector<double> a(n * n, 1);
     std::vector<double> b(n, 1);
     if (!procRank) {
@@ -180,21 +162,15 @@ TEST(Parallel_MPI, Test_4) {
         timeSequential = MPI_Wtime() - timeSequential;
         ASSERT_EQ(ans.first, seq.first);
         if (!ans.first) return;
-        double mse = 0;
-        for (size_t i = 0; i < n; ++i) {
-            mse += (ans.second[i] - seq.second[i]) * (ans.second[i] - seq.second[i]);
-        }
-        mse = sqrt(mse);
-        ASSERT_LE(mse, EPS);
         double err = 0;
         for (size_t i = 0; i < n; ++i) {
             double su = 0;
             for (size_t j = 0; j < n; ++j) {
                 su += a[i * n + j] * ans.second[j];
             }
-            ASSERT_LE(fabs(su - b[i]), sqrt(eps));
-            err += fabs(su - b[i]);
+            err += std::pow(fabs(su - b[i]), 2);
         }
+        ASSERT_LE(sqrt(err), eps);
         printf("Summary error:              %.10f\n", err);
         printf("Parallel time:              %.10f\n", timeParallel);
         printf("Sequential time:            %.10f\n", timeSequential);
@@ -205,8 +181,8 @@ TEST(Parallel_MPI, Test_5) {
     int procNum, procRank;
     MPI_Comm_size(MPI_COMM_WORLD, &procNum);
     MPI_Comm_rank(MPI_COMM_WORLD, &procRank);
-    size_t n = 1000;
-    double eps = 1e-9;
+    size_t n = 500;
+    double eps = 1e-4;
     std::vector<double> a(n * n, 1);
     std::vector<double> b(n, 1);
     if (!procRank) {
@@ -224,21 +200,15 @@ TEST(Parallel_MPI, Test_5) {
         timeSequential = MPI_Wtime() - timeSequential;
         ASSERT_EQ(ans.first, seq.first);
         if (!ans.first) return;
-        double mse = 0;
-        for (size_t i = 0; i < n; ++i) {
-            mse += (ans.second[i] - seq.second[i]) * (ans.second[i] - seq.second[i]);
-        }
-        mse = sqrt(mse);
-        ASSERT_LE(mse, EPS);
         double err = 0;
         for (size_t i = 0; i < n; ++i) {
             double su = 0;
             for (size_t j = 0; j < n; ++j) {
                 su += a[i * n + j] * ans.second[j];
             }
-            ASSERT_LE(fabs(su - b[i]), sqrt(eps));
-            err += fabs(su - b[i]);
+            err += std::pow(fabs(su - b[i]), 2);
         }
+        ASSERT_LE(sqrt(err), eps);
         printf("Summary error:              %.10f\n", err);
         printf("Parallel time:              %.10f\n", timeParallel);
         printf("Sequential time:            %.10f\n", timeSequential);
