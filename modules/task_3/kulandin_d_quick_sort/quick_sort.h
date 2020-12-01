1db3dc784f5eaed6c22852ec7ce25a730151c0b8
Denis Kulandin <kulandin08@gmail.com>
2020-12-01 18:40:42 +0300

    new veery fast version of sort

---
+++ modules/task_3/kulandin_d_quick_sort/quick_sort.h
@@ -0,0 +1,11 @@
+// Copyright 2020 Kulandin Denis
+#ifndef MODULES_TASK_3_KULANDIN_D_QUICK_SORT_QUICK_SORT_H_
+#define MODULES_TASK_3_KULANDIN_D_QUICK_SORT_QUICK_SORT_H_
+#include <vector>
+
+int* randomVector(int sz);
+int* copyVector(int* a, size_t sz);
+void division(int* a, int l, int r, size_t* t);
+void parallelQuickSort(int* a, size_t length);
+
+#endif  // MODULES_TASK_3_KULANDIN_D_QUICK_SORT_QUICK_SORT_H_
