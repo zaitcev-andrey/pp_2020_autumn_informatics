e679039623ca72cb517d5bb3bd64047ed35ff82d
tarasov001 <nikitarasov@yahoo.com>
2020-12-04 11:20:23 +0300

    fix errors

--- modules/task_3/tarasov_n_quick_sort/quick_sort.h
+++ modules/task_3/tarasov_n_quick_sort/quick_sort.h
@@ -1,6 +1,6 @@
 // Copyright 2020 Tarasov Nikita
-#ifndef MODULES_TASK_3_TARASOV_N_QUICK_SORT_H_
-#define MODULES_TASK_3_TARASOV_N_QUICK_SORT_H_
+#ifndef MODULES_TASK_3_TARASOV_N_QUICK_SORT_QUICK_SORT_H_
+#define MODULES_TASK_3_TARASOV_N_QUICK_SORT_QUICK_SORT_H_
 #include <vector>
 
 void print_array(std::vector<int> vec);
@@ -8,4 +8,4 @@ std::vector<int> random_array(int _size);
 void quick_sort(std::vector<int> *vec, int left, int right);
 void quick_sort_mpi(std::vector<int> *vec);
 
-#endif  //MODULES_TASK_3_TARASOV_N_QUICK_SORT_H_
+#endif  // MODULES_TASK_3_TARASOV_N_QUICK_SORT_QUICK_SORT_H_
